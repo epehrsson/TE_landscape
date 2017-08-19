6d87413d7c0132bad3ef97a7c258d62f4fe6d407
Erica Pehrsson <epehrsson@wustl.edu>
2017-08-18 21:57:06 -0500

    Combined pi with mm10 stats, removed mm9 coordinates and stats
    Former-commit-id: 0ed7d54ec2f585ab5f34fbcc4c081b5e8211a14b

--- R_scripts/mouse_TE_stats.R
+++ R_scripts/mouse_TE_stats.R
@@ -1,25 +1,17 @@
 # Mouse TE statistics
-# See 5/19/2016, 2/9/2017, 5/17/2017
-
-# RepeatMasker file for mm9
-mm9_rmsk_TE = read.table("mm9_rmsk_TE.txt",sep='\t')
-colnames(mm9_rmsk_TE) = c("Chromosome","Start","Stop","Subfamily","Class","Family","Strand")
-mm9_rmsk_TE$Length = mm9_rmsk_TE$Stop - mm9_rmsk_TE$Start
-
-# RepeatMasker file for mm9, restricted to other TEs
-mm9_rmsk_other = read.table("Mouse/mm9_rmsk_other.txt",sep='\t')
-colnames(mm9_rmsk_other) = c("Chromosome","Start","Stop","Subfamily","Class","Family","Strand")
-mm9_rmsk_other$Length = mm9_rmsk_other$Stop - mm9_rmsk_other$Start
-
-# Number of instances, median, sd by class
-mm9_rmsk_TE_stats = merge(merge(aggregate(data=mm9_rmsk_TE,Length ~ Class,FUN=length),aggregate(data=mm9_rmsk_TE,Length ~ Class,FUN=median),by=c("Class")),aggregate(data=mm9_rmsk_TE,Length ~ Class,FUN=sd),by=c("Class"))
-colnames(mm9_rmsk_TE_stats) = c("Class","Count","Median","SD")
-
-# Number of instances, median, sd by class
-mm9_rmsk_other_stats = merge(merge(aggregate(data=mm9_rmsk_other,Length ~ Class,FUN=length),aggregate(data=mm9_rmsk_other,Length ~ Class,FUN=median),by=c("Class")),aggregate(data=mm9_rmsk_other,Length ~ Class,FUN=sd),by=c("Class"))
-colnames(mm9_rmsk_other_stats) = c("Class","Count","Median","SD")
+# See 5/19/2016, 9/23/2016, 2/9/2017, 5/17/2017
 
 # RepeatMasker file for mm10
 mm10_rmsk_TE = read.table("mm10_rmsk_TE.txt",sep='\t')
 colnames(mm10_rmsk_TE) = c("chromosome","start","stop","subfamily","class","family","strand")
 mm10_rmsk_TE$Length = mm10_rmsk_TE$stop - mm10_rmsk_TE$start
+
+# Number of TEs, subfamilies, and length by class, mm10
+mm10_rmsk_TE_class_pi = merge(merge(aggregate(data=mm10_rmsk_TE,Length ~ class,FUN=length),aggregate(data=mm10_rmsk_TE,subfamily ~ class,function(x) length(unique(x))),by=c("class")),aggregate(data=mm10_rmsk_TE,Length ~ class,FUN=sum),by=c("class"))
+colnames(mm10_rmsk_TE_class_pi) = c("Class","Elements","Subfamilies","Length")
+test = c("Unconfident",colSums(mm10_rmsk_TE_class_pi[which(mm10_rmsk_TE_class_pi$Class %in% c("DNA?","LINE?","LTR?","SINE?","Unknown","Unknown?")),2:4]))
+names(test)[1] = "Class"
+mm10_rmsk_TE_class_pi$Class = factor(mm10_rmsk_TE_class_pi$Class,levels=c("DNA","LINE","LTR","SINE","RC","Unconfident"))
+mm10_rmsk_TE_class_pi = rbind(mm10_rmsk_TE_class_pi[c(1,3,5,7,9),],test)
+mm10_rmsk_TE_class_pi[,2:4] = apply(mm10_rmsk_TE_class_pi[,2:4],2,function(x) as.numeric(x))
+mm10_rmsk_TE_class_pi = mm10_rmsk_TE_class_pi[c(1:3,5,4,6),]
