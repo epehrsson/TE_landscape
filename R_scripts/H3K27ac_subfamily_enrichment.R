f05e35105f000bd1e30c388cce135dc74736b0e3
Erica Pehrsson <epehrsson@wustl.edu>
2017-08-19 17:49:43 -0500

    Fixed all subfamily enrichment calculations. Moved clustering to separate scripts.
    Former-commit-id: f8497c8f28be08e847033508a805cccd78f672f5

--- R_scripts/H3K27ac_subfamily_enrichment.R
+++ R_scripts/H3K27ac_subfamily_enrichment.R
@@ -1,39 +1,56 @@
 # H3K27ac subfamily enrichment
 # See 7/4/2017, 7/24/2017
 
-# Enrichment of H3K27ac peaks (width) in TE subfamilies
+source("R_scripts/TE_subfamily_stats.R")
+source("R_scripts/H3K27ac_overlap.R")
+load("R_scripts/TE_H3K27ac_peaks.RData")
+
+# Length of H3K27ac peak overlapping subfamily per sample
 subfamily_H3K27ac_sample = read.table("H3K27ac/subfamily_H3K27ac_sample.txt",sep='\t')
-colnames(subfamily_H3K27ac_sample) = c("Subfamily","Sample","Length_ijk")
-subfamily_H3K27ac_sample_expand = expand.grid(Subfamily = levels(subfamily_state_sample$Subfamily),Sample = levels(subfamily_H3K27ac_sample$Sample))
-subfamily_H3K27ac_sample_expand = join(subfamily_H3K27ac_sample_expand,rbind(rmsk_TE_stats_subfamily,rmsk_other_stats_subfamily)[,1:3],by=c("Subfamily"))
-subfamily_H3K27ac_sample = join(subfamily_H3K27ac_sample_expand,subfamily_H3K27ac_sample,by=c("Subfamily","Sample"),type="left")
+colnames(subfamily_H3K27ac_sample) = c("subfamily","Sample","Length_ijk")
+
+# Length of subfamily
+subfamily_H3K27ac_sample_expand = expand.grid(subfamily = levels(rmsk_TE_subfamily$subfamily),Sample = levels(subfamily_H3K27ac_sample$Sample))
+subfamily_H3K27ac_sample_expand = join(subfamily_H3K27ac_sample_expand,rmsk_TE_subfamily[,c(1:3,21)],by=c("subfamily"))
+subfamily_H3K27ac_sample = join(subfamily_H3K27ac_sample_expand,subfamily_H3K27ac_sample,by=c("subfamily","Sample"),type="left")
+colnames(subfamily_H3K27ac_sample)[5] = "Length_ik"
 subfamily_H3K27ac_sample[is.na(subfamily_H3K27ac_sample$Length_ijk),]$Length_ijk = 0
-subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,H3K27ac_stats[,c(1,4)],by=c("Sample"))
-colnames(subfamily_H3K27ac_sample)[6] = "Length_jk"
-subfamily_H3K27ac_sample$Length_k = apply(subfamily_H3K27ac_sample,1,function(x) mnemonics_states_TEother[x[1],]$Total)
-subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,unique(subfamily_state_sample[,c(1:3,5,7:12,14)]),by=c("Subfamily","Family","Class","Sample"))
-subfamily_H3K27ac_sample = subfamily_H3K27ac_sample[,c(1:4,8:13,5:6,14,7)]
+rm(subfamily_H3K27ac_sample_expand)
+
+# Total length of H3K27ac peaks per sample
+subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,H3K27ac_stats[,c(1,4)],by=c("Sample"),all.x=TRUE)
+colnames(subfamily_H3K27ac_sample)[7] = "Length_jk"
+
+# Total length of sample
+mnemonics_states_genome = melt(as.matrix(t(read.table("chromHMM/genome/mnemonics_state.txt",sep='\t',header=TRUE,row.names=1)[1,])))[,c(1,3)]
+colnames(mnemonics_states_genome) = c("Sample","Length_k")
+subfamily_H3K27ac_sample = join(subfamily_H3K27ac_sample,mnemonics_states_genome,by=c("Sample"),type="left")
+rm(mnemonics_states_genome)
+
+# LOR enrichment
 subfamily_H3K27ac_sample$Enrichment = log2((subfamily_H3K27ac_sample$Length_ijk/subfamily_H3K27ac_sample$Length_ik)/(subfamily_H3K27ac_sample$Length_jk/subfamily_H3K27ac_sample$Length_k))
 
 # Total proportion of H3K27ac peaks in subfamily
 subfamily_H3K27ac_sample$Length_percent_jk = subfamily_H3K27ac_sample$Length_ijk/subfamily_H3K27ac_sample$Length_jk
 
-# Filtered with thresholds
-subfamily_H3K27ac_sample_filter = subfamily_H3K27ac_sample[which(subfamily_H3K27ac_sample$Length_ijk >= 600 & subfamily_H3K27ac_sample$Length_ik > 5000),]
+# Add metadata
+subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,metadata[,c(1,4:9)],by=c("Sample"),all.x=TRUE)
 
 # Number of members overlapping H3K27ac peak (needs matrix)
 TE_H3K27ac_peaks_members = merge(melt(aggregate(data=TE_H3K27ac_peaks[,c(4,8:105)],.~subfamily,function(x) sum(as.numeric(x))),id.vars=c("subfamily")),melt(aggregate(data=TE_H3K27ac_peaks[,c(4,8:105)],.~subfamily,function(x) sum(as.numeric(x) > 0)),id.vars=c("subfamily")),by=c("subfamily","variable"))
 colnames(TE_H3K27ac_peaks_members)[2:4] = c("Sample","H3K27ac_peaks","TEs_overlapping")
-TE_H3K27ac_peaks_members = merge(TE_H3K27ac_peaks_members,rmsk_TEother_stats_subfamily[,3:4],by.x=c("subfamily"),by.y=c("Subfamily"))
-TE_H3K27ac_peaks_members[which(TE_H3K27ac_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Count = rmsk_TEother_stats_subfamily[match(TE_H3K27ac_peaks_members[which(TE_H3K27ac_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$subfamily,rmsk_TEother_stats_subfamily$Subfamily),]$Count_noY
+TE_H3K27ac_peaks_members = merge(TE_H3K27ac_peaks_members,rmsk_TE_subfamily[,c(1,4)],by="subfamily")
+TE_H3K27ac_peaks_members[which(TE_H3K27ac_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Count = rmsk_TE_subfamily[match(TE_H3K27ac_peaks_members[which(TE_H3K27ac_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY
 
 # Proportion of members overlapping H3K27ac peak
 TE_H3K27ac_peaks_members$Percent = TE_H3K27ac_peaks_members$TEs_overlapping/TE_H3K27ac_peaks_members$Count
 
 # Added to enrichment matrix
-subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,TE_H3K27ac_peaks_members,by.x=c("Subfamily","Sample"),by.y=c("subfamily","Sample"),all.x=TRUE)
-subfamily_H3K27ac_sample[which(subfamily_H3K27ac_sample$Subfamily == "Tigger2a_Car"),c(17:18,20)] = 0
-subfamily_H3K27ac_sample[which(subfamily_H3K27ac_sample$Subfamily == "Tigger2a_Car"),]$Count = 2
+subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,TE_H3K27ac_peaks_members,by=c("subfamily","Sample"),all.x=TRUE)
+subfamily_H3K27ac_sample[which(is.na(subfamily_H3K27ac_sample$Percent)),17:20] = 0
+subfamily_H3K27ac_sample[which(subfamily_H3K27ac_sample$subfamily == "Tigger2a_Car"),]$Count = 2
+subfamily_H3K27ac_sample = subfamily_H3K27ac_sample[,c(1,3:4,2,6,5,7:20)]
 
 # Number of samples subfamily is enriched
-subfamily_H3K27ac_sample_counts = ddply(subfamily_H3K27ac_sample,.(Class,Family,Subfamily),function(x) sum(x$Enrichment > 1.5 & x$Length_ijk >= 600 & x$Length_ik > 5000))
+subfamily_H3K27ac_sample_counts = ddply(subfamily_H3K27ac_sample,.(class_update,family,subfamily),function(x) sum(x$Enrichment > 1.5 & x$Length_ijk >= 600 & x$Length_ik > 5000))
+subfamily_H3K27ac_sample_counts$State = rep("H3K27ac",968)
--- R_scripts/WGBS_subfamily_enrichment_CpG.R
+++ R_scripts/WGBS_subfamily_enrichment_CpG.R
@@ -1,54 +1,32 @@
 # Enrichment of hypomethylated CpGs in a subfamily
 # See 2/6/2017, 7/21/2017, 7/23/2017, 7/26/2017, 8/2/2017
 
+source("R_scripts/TE_subfamily_stats.R")
+source("R_scripts/WGBS_sample_CpG_state.R")
+
 # Proportion of subfamily CpGs in methylation state by sample
 subfamily_CpG_meth = read.table("WGBS/subfamily_CpG_Meth_states.txt",sep='\t')
-subfamily_CpG_meth$V1 = mapvalues(subfamily_CpG_meth$V1,seq(4,40,1),as.vector(EID_metadata_meth$Sample))
+subfamily_CpG_meth$V1 = mapvalues(subfamily_CpG_meth$V1,seq(4,40,1),as.vector(metadata[which(!is.na(metadata$WGBS)),]$Sample))
 subfamily_CpG_meth = subfamily_CpG_meth[,c(1,6,2:5)]
 colnames(subfamily_CpG_meth) = c("Sample","subfamily",meth_states)
 subfamily_CpG_meth[is.na(subfamily_CpG_meth)] = 0
 
-subfamily_CpG_meth = merge(subfamily_CpG_meth,TE_subfamily_CpG_count[,1:4],by.x=c("subfamily"),by.y=c("Subfamily"),all.x=TRUE)
+# CpGs per subfamily
+subfamily_CpG_meth = merge(subfamily_CpG_meth,rmsk_TE_subfamily[,c(1:3,20)],by=c("subfamily"),all.x=TRUE)
 
-# Proportion of all CpGs hypomethylated
+# Proportion of all CpGs hypomethylated by sample
 subfamily_CpG_meth$Hypomethylated_all = apply(subfamily_CpG_meth,1,function(x) all_CpG_meth[x[2],]$Hypomethylated)
 
 # Enrichment of hypomethylated CpGs in sample x subfamily
 subfamily_CpG_meth$Enrichment = log2((subfamily_CpG_meth$Hypomethylated/subfamily_CpG_meth$CpGs)/(subfamily_CpG_meth$Hypomethylated_all/56434896))
 
-# With thresholds 
-subfamily_CpG_meth[which(subfamily_CpG_meth$Enrichment > 1.5 & subfamily_CpG_meth$CpGs >= 25 & subfamily_CpG_meth$Hypomethylated >= 6),]
-
-# Adding Unconfident class
-subfamily_CpG_meth$class_update = subfamily_CpG_meth$Class
-subfamily_CpG_meth$class_update = factor(subfamily_CpG_meth$class_update,levels=c("DNA","LINE","LTR","SINE","RC","Other","Unconfident"))
-subfamily_CpG_meth[which(subfamily_CpG_meth$Class %in% c("DNA?","LINE?","LTR?","SINE?","Unknown","Unknown?")),]$class_update = "Unconfident"
-
-# Proprotion of all hypomethylated CpGs in subfamily
+# Proportion of all hypomethylated CpGs in subfamily
 subfamily_CpG_meth$Hypo_percent = subfamily_CpG_meth$Hypomethylated/subfamily_CpG_meth$Hypomethylated_all
 
-# Number of hypomethylation enrichments per subfamily
-subfamily_hypo_sample_counts = ddply(subfamily_CpG_meth,.(Class,Family,subfamily),function(x) sum(x$Enrichment > 1.5 & x$CpGs >= 25 & x$Hypomethylated >= 6))
-
 # Adding metadata
-subfamily_CpG_meth = merge(subfamily_CpG_meth,EID_metadata[,c(1,5,7:11)],by=c("Sample"),all.x=TRUE)
+subfamily_CpG_meth = merge(subfamily_CpG_meth,metadata[,c(1,4:9)],by=c("Sample"),all.x=TRUE)
+subfamily_CpG_meth = subfamily_CpG_meth[,c(2,7:8,1,3:6,9:18)]
 
-# Finding proportion of sample groupings enriched
-enrichment_proportion_age = aggregate(data=subfamily_CpG_meth[which(subfamily_CpG_meth$CpGs >= 25 & subfamily_CpG_meth$Hypomethylated >= 6),],Enrichment~subfamily+Age,function(x) sum(x > 1.5))
-enrichment_proportion_anatomy = aggregate(data=subfamily_CpG_meth[which(subfamily_CpG_meth$CpGs >= 25 & subfamily_CpG_meth$Hypomethylated >= 6),],Enrichment~subfamily+Anatomy,function(x) sum(x > 1.5))
-enrichment_proportion_germline = aggregate(data=subfamily_CpG_meth[which(subfamily_CpG_meth$CpGs >= 25 & subfamily_CpG_meth$Hypomethylated >= 6),],Enrichment~subfamily+Germline,function(x) sum(x > 1.5))
-enrichment_proportion_group = aggregate(data=subfamily_CpG_meth[which(subfamily_CpG_meth$CpGs >= 25 & subfamily_CpG_meth$Hypomethylated >= 6),],Enrichment~subfamily+Group,function(x) sum(x > 1.5))
-enrichment_proportion_type = aggregate(data=subfamily_CpG_meth[which(subfamily_CpG_meth$CpGs >= 25 & subfamily_CpG_meth$Hypomethylated >= 6),],Enrichment~subfamily+Type,function(x) sum(x > 1.5))
-enrichment_proportion_age$Metadata = rep("Age",dim(enrichment_proportion_age)[1])
-enrichment_proportion_anatomy$Metadata = rep("Anatomy",dim(enrichment_proportion_anatomy)[1])
-enrichment_proportion_germline$Metadata = rep("Germline",dim(enrichment_proportion_germline)[1])
-enrichment_proportion_group$Metadata = rep("Group",dim(enrichment_proportion_group)[1])
-enrichment_proportion_type$Metadata = rep("Type",dim(enrichment_proportion_type)[1])
-colnames(enrichment_proportion_age)[2] = "Category"
-colnames(enrichment_proportion_anatomy)[2] = "Category"
-colnames(enrichment_proportion_germline)[2] = "Category"
-colnames(enrichment_proportion_group)[2] = "Category"
-colnames(enrichment_proportion_type)[2] = "Category"
-enrichment_proportion_test = rbind(enrichment_proportion_age,enrichment_proportion_anatomy,enrichment_proportion_germline,enrichment_proportion_group,enrichment_proportion_type)
-enrichment_proportion_test$Proportion = apply(enrichment_proportion_test,1,function(x) as.numeric(x[3])/length(EID_metadata_meth[which(EID_metadata_meth[,x[4]] == x[2]),x[4]]))
-enrichment_proportion_test[which(enrichment_proportion_test$Proportion > 0.5 & enrichment_proportion_test$Enrichment > 1),] #95 entries
+# Number of hypomethylation enrichments per subfamily
+subfamily_hypo_sample_counts = ddply(subfamily_CpG_meth,.(class_update,family,subfamily),function(x) sum(x$Enrichment > 1.5 & x$CpGs >= 25 & x$Hypomethylated >= 6))
+subfamily_hypo_sample_counts$State = rep("Hypomethylated",968)
