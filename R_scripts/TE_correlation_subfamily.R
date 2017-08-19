a7897ba78d57f9b10f13e120e3c561d1b2c2ff82
Erica Pehrsson <epehrsson@wustl.edu>
2017-08-18 22:38:22 -0500

    WGBS TE hypo/hyper analysis - no longer using TE-level enrichment, just as comparison to age
    Former-commit-id: 76e6e65db129acc8e7d0d658627d0c757146d1e6

--- R_scripts/TE_correlation_subfamily.R
+++ R_scripts/TE_correlation_subfamily.R
@@ -1,20 +1,24 @@
-# Hypomethylated proportion mean, max per subfamily
-rmsk_TEother_age_subfamily = merge(rmsk_TEother_age_subfamily,TE_meth_subfamily_hypo[,c(1:3,41:42,45:46)],by=c("subfamily","family","class"),all.x=TRUE)
-colnames(rmsk_TEother_age_subfamily)[29:30] = c("Mean_hypo","Mean_hypo_noIMR90")
-colnames(rmsk_TEother_age_subfamily)[31:32] = c("Max_hypo","Max_hypo_noIMR90")
+# Correlation of TE subfamily features with number of samples enriched in state
+
+source("R_scripts/TE_subfamily_stats.R")
+
+# Hypomethylated proportion mean, max, range per subfamily
+source('~/TE_landscape/R_scripts/WGBS_subfamily_enrichment_TE.R')
+
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,TE_meth_subfamily_hypo[,c(1:3,42:47)],by=c("subfamily","family","class_update"),all.x=TRUE)
 
 # Number of samples enriched by subfamily
 # Adding number of samples enriched in chromHMM states
-rmsk_TEother_age_subfamily = merge(rmsk_TEother_age_subfamily,dcast(subfamily_state_sample_counts,Class+Family+Subfamily~State,value.var=c("V1"))[,3:18],by.x="subfamily",by.y="Subfamily",all.x=TRUE)
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,dcast(subfamily_state_sample_counts,Class+Family+Subfamily~State,value.var=c("V1"))[,3:18],by.x="subfamily",by.y="Subfamily",all.x=TRUE)
 
 # Correlation between hypomethylated CpG enrichment, age
-rmsk_TEother_age_subfamily = merge(rmsk_TEother_age_subfamily,subfamily_hypo_sample_counts[,3:4],by=c("subfamily"))
-colnames(rmsk_TEother_age_subfamily)[33] = "CpG_Hypo"
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,subfamily_hypo_sample_counts[,3:4],by=c("subfamily"))
+colnames(rmsk_TE_subfamily)[33] = "CpG_Hypo"
 
 # Adding Dnase enrichment
-rmsk_TEother_age_subfamily = merge(rmsk_TEother_age_subfamily,subfamily_DNase_sample_counts[,3:4],by.x="subfamily",by.y="Subfamily",all.x=TRUE)
-colnames(rmsk_TEother_age_subfamily)[32] = "DNase"
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,subfamily_DNase_sample_counts[,3:4],by.x="subfamily",by.y="Subfamily",all.x=TRUE)
+colnames(rmsk_TE_subfamily)[32] = "DNase"
 
 # Adding H3K27ac
-rmsk_TEother_age_subfamily = merge(rmsk_TEother_age_subfamily,subfamily_H3K27ac_sample_counts[,3:4],by.x="subfamily",by.y="Subfamily",all.x=TRUE)
-colnames(rmsk_TEother_age_subfamily)[28] = "H3K27ac"
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,subfamily_H3K27ac_sample_counts[,3:4],by.x="subfamily",by.y="Subfamily",all.x=TRUE)
+colnames(rmsk_TE_subfamily)[28] = "H3K27ac"
