f54fb626d067f0287e9cb1ef1d20c392f217a119
Erica Pehrsson <epehrsson@wustl.edu>
2017-08-20 12:24:49 -0500

    Updated mouse ortholog stats, mouse Figure
    Former-commit-id: f8e34dcdf07f39db22b04435e5436fb6405d7784

--- R_scripts/TE_ortholog_class_stats.R
+++ R_scripts/TE_ortholog_class_stats.R
@@ -6,11 +6,11 @@ source("R_scripts/TE_class_stats.R")
 human_mouse_orthologs_mm10 = read.table("Mouse/liftover/hg19_mm10_TE_intersect_same.bed",sep='\t')
 colnames(human_mouse_orthologs_mm10) = c("human_chr_mm10","human_start_mm10","human_stop_mm10","human_strand_mm10","human_chr_hg19","human_start_hg19","human_stop_hg19","human_subfamily","human_class","human_family","human_strand_hg19","mouse_chr_mm10","mouse_start_mm10","mouse_stop_mm10","mouse_subfamily","mouse_class","mouse_family","mouse_strand_mm10","overlap")
 
-test = table(human_mouse_orthologs_mm10$human_class)
+test = table(unique(human_mouse_orthologs_mm10[,5:11])$human_class)
 test[12] = sum(test[c(2,4,6:7,9:11)])
 names(test)[12] = "Other"
-rmsk_TE_class$Mouse_ortholog_class = test[as.vector(rmsk_TE_class$class_update)]
-rmsk_TE_class[which(is.na(rmsk_TE_class$Mouse_ortholog_class)),]$Mouse_ortholog_class = 0
+rmsk_TE_class$Mouse_orthologs = test[as.vector(rmsk_TE_class$class_update)]
+rmsk_TE_class[which(is.na(rmsk_TE_class$Mouse_orthologs)),]$Mouse_orthologs = 0
 rm(test)
 
 mm10_rmsk_TE = read.table("features/mouse/TEs/mm10_rmsk_TE.txt",sep='\t')
@@ -18,5 +18,5 @@ colnames(mm10_rmsk_TE) = c("chromosome","start","stop","subfamily","class","fami
 
 source("R_scripts/TE_subfamily_stats.R")
 
-rmsk_TE_class = merge(rmsk_TE_class,ddply(rmsk_TE_subfamily[which(rmsk_TE_subfamily$subfamily %in% mm10_rmsk_TE$subfamily),],~class_update,summarize,Mouse_ortholog_subfamily = length(unique(subfamily))),by="class_update",all.x=TRUE)
+rmsk_TE_class = merge(rmsk_TE_class,ddply(rmsk_TE_subfamily[which(rmsk_TE_subfamily$subfamily %in% mm10_rmsk_TE$subfamily),],~class_update,summarize,Mouse_ortholog_subfamily = length(subfamily)),by="class_update",all.x=TRUE)
 rmsk_TE_class[which(is.na(rmsk_TE_class$Mouse_ortholog_subfamily)),]$Mouse_ortholog_subfamily = 0
