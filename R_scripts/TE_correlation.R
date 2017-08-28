c05ea12d6d5e904658f6ab98abb063db3660eafb
Erica Pehrsson <epehrsson@wustl.edu>
2017-08-27 19:55:48 -0500

    Converted features to yes/no, changed class contrast, added RNA
    Former-commit-id: 25906b319475df1ae91dc8d071e2038a8696e7c7

--- R_scripts/TE_correlation.R
+++ R_scripts/TE_correlation.R
@@ -7,22 +7,35 @@ load("R_scripts/chromHMM_TE_state.RData")
 load("R_scripts/TE_meth_average.RData")
 load("R_scripts/TE_DNase_peaks.RData")
 load("R_scripts/TE_H3K27ac_peaks.RData")
+load("R_scripts/rna.RData")
+
+rmsk_TE_measure = rmsk_TE
+contrasts(rmsk_TE_measure$class_update) <- contr.sum
+
+# Convert feature overlap "NA" to 0 - or categories
+rmsk_TE_measure[is.na(rmsk_TE_measure)] = 0
+rmsk_TE_measure[,13:32] = apply(rmsk_TE_measure[,13:32],2,function(x) {ifelse(x == 0, "no", "yes")})
+rmsk_TE_measure[,13:32] = apply(rmsk_TE_measure[,13:32],2,function(x) x <- factor(x,levels=c("no","yes")))
 
 # Add number of samples in chromHMM state per TE
-rmsk_TE_measure = merge(rmsk_TE,chromHMM_TE_state[,c(1:23,25)],by=c("chromosome","start","stop","subfamily","family","class","strand"))
+rmsk_TE_measure = merge(rmsk_TE_measure,chromHMM_TE_state[,c(1:23,25)],by=c("chromosome","start","stop","subfamily","family","class","strand"))
 
 # Add number of samples in methylation state per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,TE_meth_average[,c(1:7,45:53)],by=c("chromosome","start","stop","subfamily","family","class","strand"),all.x=TRUE)
 rmsk_TE_measure[which(is.na(rmsk_TE_measure$CpGs)),]$CpGs = 0
+rmsk_TE_measure$CpGs_per_length = rmsk_TE_measure$CpGs/rmsk_TE_measure$Length
 
 # Add number of samples overlapping DNase peak per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,TE_DNase_peaks[,c(1:7,62)],by=c("chromosome","start","stop","subfamily","family","class","strand"),all.x=TRUE)
-colnames(rmsk_TE_measure)[48] = "DNase"
+colnames(rmsk_TE_measure)[length(colnames(rmsk_TE_measure))] = "DNase"
 rmsk_TE_measure[which(is.na(rmsk_TE_measure$DNase)),]$DNase = 0
 
 # Add number of samples overlapping H3K27ac peak per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,TE_H3K27ac_peaks[,c(1:7,107)],by=c("chromosome","start","stop","subfamily","family","class","strand"),all.x=TRUE)
-colnames(rmsk_TE_measure)[49] = "H3K27ac"
+colnames(rmsk_TE_measure)[length(colnames(rmsk_TE_measure))] = "H3K27ac"
 rmsk_TE_measure[which(is.na(rmsk_TE_measure$H3K27ac)),]$H3K27ac = 0
 
-rmsk_TE_measure = rmsk_TE_measure[,c(1:7,9,8,39,10:38,40:49)]
+# Add number of samples expressed per TE
+rmsk_TE_measure = merge(rmsk_TE_measure,RNA_TE_agnostic[,c(1:7,61:62)],by=c("chromosome","start","stop","subfamily","family","class","strand"))
+
+rmsk_TE_measure = rmsk_TE_measure[,c(1:12,50,59,13:32,33:49,51:58,60:63)]
