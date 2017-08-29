50041045cee6617459fac74ebc04867ce7441d6a
Erica Pehrsson <epehrsson@wustl.edu>
2017-08-29 11:07:06 -0500

    Added pc/nc features, contrasts, chromHMM %, RNA

--- R_scripts/TE_correlation_subfamily.R
+++ R_scripts/TE_correlation_subfamily.R
@@ -3,6 +3,7 @@
 library(reshape2)
 
 source("R_scripts/TE_subfamily_stats.R")
+load("R_datasets/rna.RData")
 
 # Number of samples enriched by subfamily
 source("R_scripts/chromHMM_subfamily_enrichment.R")
@@ -10,19 +11,24 @@ source("R_scripts/WGBS_subfamily_enrichment_CpG.R")
 source("R_scripts/DNase_subfamily_enrichment.R")
 source("R_scripts/H3K27ac_subfamily_enrichment.R")
 
-# Adding number of samples enriched in chromHMM states
-rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily,dcast(subfamily_state_sample_counts,class_update+family+subfamily~State,value.var=c("V1"))[,c(3,10:17,4:9)],by="subfamily",all.x=TRUE)
+source('~/TE_landscape/R_scripts/WGBS_subfamily_enrichment_TE.R')
 
-# How to add chromHMM stats?
-ddply(subfamily_state_sample,~subfamily+State,summarise,State_Max = max(Percent),State_Mean = mean(Percent),State_Range = (max(Percent)-min(Percent)))
+# Adding number of samples enriched in chromHMM states
+rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily,dcast(subfamily_state_sample_counts,class_update+family+subfamily~State,value.var=c("V1"))[,c(3:4,11:18,5:10)],by="subfamily",all.x=TRUE)
+
+# chromHMM proportion mean, max, range per subfamily
+test = melt(ddply(subfamily_state_sample,~subfamily+State,summarise,Max = max(Percent),Mean = mean(Percent),Range = (max(Percent)-min(Percent))),id.vars=c("subfamily","State"))
+test$Header = apply(test,1,function(x) paste(x[2],x[3],sep="_"))
+test = dcast(test[,c(1,4:5)],subfamily~Header)
+rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,test,by="subfamily")
+rm(test)
 
 # Adding hypomethylated CpG enrichment
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,dcast(subfamily_hypo_sample_counts,class_update+family+subfamily~State,value.var=c("V1"))[,3:4],by="subfamily",all.x=TRUE)
 
-# Hypomethylated proportion mean, max, range per subfamily
-source('~/TE_landscape/R_scripts/WGBS_subfamily_enrichment_TE.R')
-
+# Hypomethylated proportion mean, max, range per subfamily (should be CpGs)
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,TE_meth_subfamily_hypo[,c(1:3,42:47)],by=c("subfamily","family","class_update"),all.x=TRUE)
+colnames(rmsk_TE_subfamily_measure)[97:102] = c("Hypo_Mean","Hypo_Mean_noIMR90","Hypo_Range","Hypo_Range_noIMR90","Hypo_Max","Hypo_Max_noIMR90")
 
 # Adding Dnase enrichment
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,dcast(subfamily_DNase_sample_counts,class_update+family+subfamily~State,value.var=c("V1"))[,3:4],by="subfamily",all.x=TRUE)
@@ -35,3 +41,6 @@ rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,dcast(subfamily_H3K2
 
 # Add H3K27ac proportion mean, max, range
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,ddply(subfamily_H3K27ac_sample,~subfamily,summarise,H3K27ac_Max = max(Percent),H3K27ac_Mean = mean(Percent),H3K27ac_Range = (max(Percent)-min(Percent))),by="subfamily")
+
+# Adding RNA expression 
+rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,RNA_TE_agnostic_subfamily[,c(3,57:58)],by="subfamily")
--- R_scripts/TE_subfamily_stats.R
+++ R_scripts/TE_subfamily_stats.R
@@ -1,14 +1,14 @@
 # TE statistics by subfamily
 # See 4/19/2016, 4/25/2016, 8/24/2016, 8/25/2016, 9/20/2016, 9/21/2016, 9/27/2016, 9/28/2016, 11/4/2016, 11/5/2016, 11/7/2016, 11/18/2016, 12/16/2016, 1/31/2017, 2/1/2017, 2/3/2017, 2/6/2017, 2/9/2017, 2/10/2017, 2/25/2017, 2/27/2017, 2/28/2017, 3/5/2017, 3/8/2017, 5/14/2017, 5/15/2017, 5/16/2017, 5/17/2017, 6/7/2017, 6/14/2017, 6/15/2017, 7/21/2017, 7/24/2017, 8/1/2017, 8/2/2017
 
-load("R_scripts/rmsk_TE.RData")
+load("R_datasets/rmsk_TE.RData")
 
 # Number of instances, median/sd of length, mean/sd mappability and age by subfamily
 rmsk_TE_subfamily = ddply(rmsk_TE,~subfamily+family+class_update,summarize,Count = length(Length),Median_length = median(Length), SD_length = sd(Length), Mappability = mean(mappability), Mappability_SD = sd(mappability), Age = mean(JC_distance), Age_SD = sd(JC_distance))
 rmsk_TE_subfamily = merge(rmsk_TE_subfamily,ddply(rmsk_TE[which(rmsk_TE$chromosome != "chrY"),],~subfamily+family+class_update,summarize,Count_noY = length(Length)),by=c("subfamily","family","class_update"))
 
 # Proportion of each subfamily in each feature
-rmsk_TE_subfamily = merge(rmsk_TE_subfamily,ddply(rmsk_TE,~subfamily,function(y) apply(y[,13:20],2,function(x) length(na.omit(x))/length(x))),by="subfamily")
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,ddply(rmsk_TE,~subfamily,function(y) apply(y[,13:32],2,function(x) length(na.omit(x))/length(x))),by="subfamily")
 
 # Number of CpGs per subfamily
 TE_subfamily_CpG_count = read.table("WGBS/subfamily/TE_CpG_subfamily.txt",sep='\t')
