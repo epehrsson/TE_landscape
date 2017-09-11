aa2e5d16084d8ec82e765834aad8593293b0c449
Erica Pehrsson <epehrsson@wustl.edu>
2017-09-11 17:36:21 -0500

    Changed proportion to total across samples, not average

--- R_scripts/proportion.R
+++ R_scripts/proportion.R
@@ -8,23 +8,25 @@ library(reshape2)
 # Genome
 # Number of bases in each state in each sample overall
 mnemonics_states_genome = read.table("chromHMM/genome/mnemonics_state.txt",sep='\t',header=TRUE,row.names=1)
+mnemonics_states_genome$Total = rowSums(mnemonics_states_genome)
 
 # Normalized by total annotated
 mnemonics_states_genome_normalized = adply(mnemonics_states_genome[2:16,],1,function(x) x/mnemonics_states_genome[1,])
 rownames(mnemonics_states_genome_normalized) = chromHMM_states
-mnemonics_states_genome_normalized$Mean = rowMeans(mnemonics_states_genome_normalized)
-mnemonics_states_genome_normalized$SD = apply(mnemonics_states_genome_normalized,1,sd)
+mnemonics_states_genome_normalized$Mean = rowMeans(mnemonics_states_genome_normalized[,1:127])
+mnemonics_states_genome_normalized$SD = apply(mnemonics_states_genome_normalized[,1:127],1,sd)
 
 # TEs
 # Number of bases in each state in each sample in merged TEs
 mnemonics_states_TE = read.table("chromHMM/mnemonics_TEother_merge_states.txt",sep='\t',header=TRUE,row.names=1)
 mnemonics_states_TE[is.na(mnemonics_states_TE)] = 0
+mnemonics_states_TE$Total = rowSums(mnemonics_states_TE)
 
 # Normalized by total annotated
 mnemonics_states_TE_normalized = adply(mnemonics_states_TE[2:16,],1,function(x) x/mnemonics_states_TE[1,])
 rownames(mnemonics_states_TE_normalized) = chromHMM_states
-mnemonics_states_TE_normalized$Mean = rowMeans(mnemonics_states_TE_normalized)
-mnemonics_states_TE_normalized$SD = apply(mnemonics_states_TE_normalized,1,sd)
+mnemonics_states_TE_normalized$Mean = rowMeans(mnemonics_states_TE_normalized[,1:127])
+mnemonics_states_TE_normalized$SD = apply(mnemonics_states_TE_normalized[,1:127],1,sd)
 
 # Refseq features   #UPDATE - Current intergenic d.n. include TEs OR repeats; remove genome_noTE, repeats
 # Number of bases in each state in each sample in merged genic features 
@@ -54,11 +56,17 @@ rm(test)
 mnemonics_states_features_noTE_normalized = merge(aggregate(data = mnemonics_states_features_noTE,Proportion~Cohort+State,mean),aggregate(data = mnemonics_states_features_noTE,Proportion~Cohort+State,sd),by=c("Cohort","State"))
 colnames(mnemonics_states_features_noTE_normalized)[3:4] = c("Mean","SD")
 
+# Normalized across all samples
+states_features_noTE = merge(aggregate(data=mnemonics_states_features_noTE,Bases~Cohort+State,sum),aggregate(data=mnemonics_states_features_noTE,Bases~Cohort,sum),by=c("Cohort"),all.x=TRUE)
+colnames(states_features_noTE)[3:4] = c("Bases_state","Bases_all")
+states_features_noTE$Proportion = states_features_noTE$Bases_state/states_features_noTE$Bases_all
+
 # Combining into one dataframe
-mnemonics_states_all = rbind(mnemonics_states_genome_normalized[,128:129],mnemonics_states_TE_normalized[,128:129])
+mnemonics_states_all = as.data.frame(c(mnemonics_states_genome_normalized$Total,mnemonics_states_TE_normalized$Total))
+colnames(mnemonics_states_all)[1] = "Proportion"
 mnemonics_states_all$State = rep(chromHMM_states,2)
 mnemonics_states_all$Cohort = c(rep("Genome",15),rep("TE",15))
-mnemonics_states_all = rbind(mnemonics_states_all,mnemonics_states_features_noTE_normalized)
+mnemonics_states_all = rbind(mnemonics_states_all,states_features_noTE[,c(1:2,5)])
 mnemonics_states_all$State = factor(mnemonics_states_all$State,levels=chromHMM_states[c(1:3,6:7,4:5,8,10:12,9,13:14,15)])
 
 # WGBS proportion 
@@ -71,7 +79,7 @@ CpG_Meth = as.data.frame(rbind(CpG_Meth,aggregate(data=feature_CpG_meth[,1:5],.~
 rownames(CpG_Meth) = CpG_Meth$Cohort
 CpG_Meth = CpG_Meth[,1:4]
 CpG_Meth = melt(as.matrix(CpG_Meth/rowSums(CpG_Meth)))
-colnames(CpG_Meth) = c("Cohort","State","Mean")
+colnames(CpG_Meth) = c("Cohort","State","Proportion")
 
 # DNase proportion
 source("~/TE_landscape/R_scripts/DNase_overlap.R")
@@ -80,12 +88,12 @@ source("~/TE_landscape/R_scripts/DNase_overlap.R")
 DNase_features = merge(DNase_features,aggregate(data=mnemonics_states_features_noTE,Bases~Cohort+Sample,sum),by=c("Sample","Cohort"))
 colnames(DNase_features)[3:4] = c("Total_width_in_feature","Feature_width")
 DNase_features$Mean = DNase_features$Total_width_in_feature/DNase_features$Feature_width
-DNase_proportion = aggregate(data=DNase_features,Mean~Cohort,mean)
 
-# Proportion of TEs overlapping DNase peaks, averaged
-DNase_proportion[20,] = c("Genome",mean(DNase_stats$Total_width/as.numeric(mnemonics_states_genome[1,as.vector(DNase_stats$Sample)])))
-DNase_proportion[21,] = c("TE",mean(DNase_stats$Total_width_in_TE/as.numeric(mnemonics_states_TE[1,as.vector(DNase_stats$Sample)])))
-DNase_proportion$Mean = as.numeric(DNase_proportion$Mean)
+# Proportion of TEs overlapping DNase peaks, across all samples
+DNase_proportion = aggregate(data=DNase_features[,2:4],.~Cohort,sum)
+DNase_proportion[20,] = c("Genome",sum(as.numeric(DNase_stats$Total_width)),sum(as.numeric(mnemonics_states_genome[1,as.vector(DNase_stats$Sample)])))
+DNase_proportion[21,] = c("TE",sum(as.numeric(DNase_stats$Total_width_in_TE)),sum(as.numeric(mnemonics_states_TE[1,as.vector(DNase_stats$Sample)])))
+DNase_proportion$Proportion = as.numeric(DNase_proportion$Total_width_in_feature)/as.numeric(DNase_proportion$Feature_width)
 DNase_proportion$State = rep("DNase",dim(DNase_proportion)[1])
 
 # H3K27ac proportion
@@ -95,16 +103,16 @@ source("~/TE_landscape/R_scripts/H3K27ac_overlap.R")
 H3K27ac_features = merge(H3K27ac_features,aggregate(data=mnemonics_states_features_noTE,Bases~Cohort+Sample,sum),by=c("Sample","Cohort"))
 colnames(H3K27ac_features)[3:4] = c("Total_width_in_feature","Feature_width")
 H3K27ac_features$Mean = H3K27ac_features$Total_width_in_feature/H3K27ac_features$Feature_width
-H3K27ac_proportion = aggregate(data=H3K27ac_features,Mean~Cohort,mean)
 
-# Proportion of TEs overlapping H3K27ac peaks, averaged
-H3K27ac_proportion[20,] = c("Genome",mean(H3K27ac_stats$Total_width/as.numeric(mnemonics_states_genome[1,as.vector(H3K27ac_stats$Sample)])))
-H3K27ac_proportion[21,] = c("TE",mean(H3K27ac_stats$Total_width_in_TE/as.numeric(mnemonics_states_TE[1,as.vector(H3K27ac_stats$Sample)])))
-H3K27ac_proportion$Mean = as.numeric(H3K27ac_proportion$Mean)
+# Proportion of TEs overlapping H3K27ac peaks, across all samples
+H3K27ac_proportion = aggregate(data=H3K27ac_features[,2:4],.~Cohort,sum)
+H3K27ac_proportion[20,] = c("Genome",sum(as.numeric(H3K27ac_stats$Total_width)),sum(as.numeric(mnemonics_states_genome[1,as.vector(H3K27ac_stats$Sample)])))
+H3K27ac_proportion[21,] = c("TE",sum(as.numeric(H3K27ac_stats$Total_width_in_TE)),sum(as.numeric(mnemonics_states_TE[1,as.vector(H3K27ac_stats$Sample)])))
+H3K27ac_proportion$Proportion = as.numeric(H3K27ac_proportion$Total_width_in_feature)/as.numeric(H3K27ac_proportion$Feature_width)
 H3K27ac_proportion$State = rep("H3K27ac",dim(H3K27ac_proportion)[1])
 
 # Combine all
-combined_proportion = rbind(mnemonics_states_all[,c(1,3:4)],CpG_Meth,DNase_proportion,H3K27ac_proportion)
+combined_proportion = rbind(mnemonics_states_all,CpG_Meth,DNase_proportion[,c(1,4:5)],H3K27ac_proportion[,c(1,4:5)])
 combined_proportion$Group = factor(c(rep("chromHMM",315),rep("WGBS",80),rep("DNase",21),rep("H3K27ac",21)),levels=c("chromHMM","WGBS","DNase","H3K27ac"))
 combined_proportion$Cohort = gsub("coding_exon","CDS",combined_proportion$Cohort)
 combined_proportion$Cohort = factor(combined_proportion$Cohort,levels=c("TE","Genome","genome_noTE","promoters","promoters_pc","promoters_nc","5UTR","5UTR_pc","5UTR_nc","CDS","CDS_pc","3UTR","3UTR_pc","3UTR_nc","exons","exons_pc","exons_nc","introns","introns_pc","introns_nc","intergenic"))
