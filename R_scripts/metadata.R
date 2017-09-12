2504dad492c4cc6089724de0ef8cd49272be06d0
Erica Pehrsson <epehrsson@wustl.edu>
2017-09-12 16:07:30 -0500

    Added samples missing chrY, fixed exclusion

--- R_scripts/metadata.R
+++ R_scripts/metadata.R
@@ -38,8 +38,12 @@ RNA_samples$RNA = rep("RNA",dim(RNA_samples)[2])
 metadata = merge(metadata,RNA_samples,by.x="Sample",by.y="V1",all.x=TRUE)
 rm(RNA_samples)
 
+# Add whether sample includes chrY
+metadata$chrY = rep("Yes",127)
+metadata[which(metadata$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$chrY = "No"
+
 # Add whether sample is IMR90 or cancer cell line
 metadata$Exclude = rep("Include",127)
-metadata[which(metadata$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Exclude = "Exclude"
+metadata[which(metadata$Sample %in% c("E017","E114","E115","E117","E118","E123")),]$Exclude = "Exclude"
 
 save(metadata,file="R_datasets/metadata.RData")
