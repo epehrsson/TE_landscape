6492bebd896fda23c158be8dcb7d6ba46c9b5dc4
Erica Pehrsson <epehrsson@wustl.edu>
2017-09-14 11:41:43 -0500

    Added WGBS and by-class analysis

--- R_scripts/state_switching.R
+++ R_scripts/state_switching.R
@@ -1,26 +1,7 @@
-# Probability that a TE ever in a particular state will be in another state in a different sample
+# State switching
 # See 7/5/2016, 7/7/2016, 2/6/2017, 2/9/2017
 
-#Format TEs
-ss_inter_TE = read.table("chromHMM/state_switching/all_chromHMM_TE_state_inter.txt",sep='\t',header=TRUE,row.names=1)
-ss_inter_TE[lower.tri(ss_inter_TE)] = t(ss_inter_TE)[lower.tri(ss_inter_TE)]
-
-#Format other TEs
-ss_inter_other = read.table("chromHMM/state_switching/all_chromHMM_other_state_inter.txt",sep='\t',header=TRUE,row.names=1)
-ss_inter_other[lower.tri(ss_inter_other)] = t(ss_inter_other)[lower.tri(ss_inter_other)]
-
-#Combine
-state_switching_inter = ss_inter_TE + ss_inter_other
-
-#Normalize
-for (i in 1:15){
-  state_switching_inter[i,] = state_switching_inter[i,]/apply(chromHMM_TE_state_dist[,2:16],2,function(x) sum(x[2:128]))[i]
-}
-
-rm(ss_inter_TE)
-rm(ss_inter_other)
-
-# Matrix of states annotated on the same TE in the same sample
+# Matrix of states annotated on the same TE in the same sample (intra)
 # Normalized by number of instances (TE x sample) each state is found, alone or with another state
 
 # Format TEs 
@@ -35,11 +16,83 @@ ss_intra_other[lower.tri(ss_intra_other)] = t(ss_intra_other)[lower.tri(ss_intra
 
 # Combine
 state_switching_intra = ss_intra_TE + ss_intra_other
+rm(ss_intra_TE)
+rm(ss_intra_other)
+
+# For Chi-sq tests
+state_switching_intra_raw = state_switching_intra
 
 #Normalize matrix
 for (i in 1:15){
   state_switching_intra[i,] = state_switching_intra[i,]/state_switching_intra[i,i]
 }
 
-rm(ss_intra_TE)
-rm(ss_intra_other)
+# Probability that a TE ever in a particular state will be in another state in a different sample (inter)
+
+# chromHMM inter state switching
+# Format TEs
+ss_inter_TE = read.table("chromHMM/state_switching/all_chromHMM_TE_state_inter.txt",sep='\t',header=TRUE,row.names=1)
+ss_inter_TE[lower.tri(ss_inter_TE)] = t(ss_inter_TE)[lower.tri(ss_inter_TE)]
+
+# Format other TEs
+ss_inter_other = read.table("chromHMM/state_switching/all_chromHMM_other_state_inter.txt",sep='\t',header=TRUE,row.names=1)
+ss_inter_other[lower.tri(ss_inter_other)] = t(ss_inter_other)[lower.tri(ss_inter_other)]
+
+# Combine
+state_switching_inter = ss_inter_TE + ss_inter_other
+rm(ss_inter_TE)
+rm(ss_inter_other)
+
+# For Chi-sq tests
+state_switching_inter_raw = state_switching_inter
+
+# Normalize
+for (i in 1:15){
+  state_switching_inter[i,] = state_switching_inter[i,]/apply(chromHMM_TE_state_dist[,2:16],2,function(x) sum(x[2:128]))[i]
+}
+
+# chromHMM, by class
+ss_inter_class = lapply(list.files(path="chromHMM/state_switching/class",full.names=TRUE),function(x) read.table(x,sep='\t',header=TRUE,row.names=1))
+names(ss_inter_class) = c("DNA","LINE","LTR","Other","SINE","SVA")
+
+for (j in 1:6){
+ ss_inter_class[[j]][lower.tri(ss_inter_class[[j]])] = t(ss_inter_class[[j]])[lower.tri(ss_inter_class[[j]])]
+ for (i in 1:15){
+   ss_inter_class[[j]][i,] = ss_inter_class[[j]][i,]/apply(chromHMM_TE_state_class[[names(ss_inter_class)[j]]][,2:16],2,function(x) sum(x[2:128]))[i]
+ }
+}
+
+ss_inter_class = ldply(ss_inter_class)
+colnames(ss_inter_class)[1] = c("Class")
+ss_inter_class$State = rep(chromHMM_states,6)
+
+# WGBS inter state switching
+# Format TEs
+ss_inter_meth = read.table("WGBS/TE_WGBS_state_inter.txt",sep='\t',header=TRUE,row.names=1)
+ss_inter_meth[lower.tri(ss_inter_meth)] = t(ss_inter_meth)[lower.tri(ss_inter_meth)]
+
+# Reorder columns and rows
+ss_inter_meth = ss_inter_meth[c(2,3,1,4),c(2,3,1,4)]
+
+# For Chi-sq tests
+ss_inter_meth_raw = ss_inter_meth
+
+# Normalize
+for (i in 1:4){
+  ss_inter_meth[i,] = ss_inter_meth[i,]/apply(TE_meth_average_category[,c(2,4,3,5)],2,function(x) sum(x[2:38]))[i]
+}
+
+# WGBS, by class
+ss_inter_meth_class = lapply(list.files(path="WGBS/class",pattern="WGBS_state_sorted.txt_inter.txt",full.names=TRUE),function(x) read.table(x,sep='\t',header=TRUE,row.names=1))
+names(ss_inter_meth_class) = c("DNA","LINE","LTR","Other","SINE","SVA")
+
+for (j in 1:6){
+  ss_inter_meth_class[[j]][lower.tri(ss_inter_meth_class[[j]])] = t(ss_inter_meth_class[[j]])[lower.tri(ss_inter_meth_class[[j]])]
+  ss_inter_meth_class[[j]] = ss_inter_meth_class[[j]][c(2,3,1,4),c(2,3,1,4)]
+  for (i in 1:4){
+    ss_inter_meth_class[[j]][i,] = ss_inter_meth_class[[j]][i,]/apply(TE_meth_average_class[[names(ss_inter_meth_class)[j]]][,c(2,4,3,5)],2,function(x) sum(x[2:38]))[i]
+  }
+}
+ss_inter_meth_class = ldply(ss_inter_meth_class)
+colnames(ss_inter_meth_class)[1] = c("Class")
+ss_inter_meth_class$State = rep(meth_states,6)
