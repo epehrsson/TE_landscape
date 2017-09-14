94f29f3c477adbd1b98aa4a01fd8cb57d84992d5
Erica Pehrsson <epehrsson@wustl.edu>
2017-09-14 14:12:10 -0500

    Converted to difference in proportion by overall proportion; added by-class analysis

--- R_scripts/feature_overlap_state.R
+++ R_scripts/feature_overlap_state.R
@@ -5,13 +5,33 @@
 source("R_scripts/TE_correlation.R")
 source("R_scripts/potential.R")
 
-feature_state_mean = apply(rmsk_TE_measure[,14:21],2,function(y) colMeans(rmsk_TE_measure[which(y != "NA"),c(23:37,40:43,48:49)],na.rm=TRUE))
+# For TEs overlapping each feature, mean number of samples in state
+feature_state_mean = apply(rmsk_TE_measure[,15:34],2,function(y) 
+  (colMeans(rmsk_TE_measure[which(y == "yes"),c(35:49,52:55,60:62)],na.rm=TRUE)-colMeans(rmsk_TE_measure[which(y == "no"),c(35:49,52:55,60:62)],na.rm=TRUE))/colMeans(rmsk_TE_measure[,c(35:49,52:55,60:62)],na.rm=TRUE))
+
 rownames(feature_state_mean)[1:15] = chromHMM_states
 feature_state_mean[1:15,] = feature_state_mean[1:15,]/1.27
 feature_state_mean[16:19,] = feature_state_mean[16:19,]/0.37
 feature_state_mean[20,] = feature_state_mean[20,]/0.53
 feature_state_mean[21,] = feature_state_mean[21,]/0.98
+feature_state_mean[22,] = feature_state_mean[22,]/0.52
 
-combine_stats = rbind(chromHMM_TE_state_dist_stats,TE_meth_average_category_stats,TE_DNase_potential_stats,TE_H3K27ac_potential_stats)
-feature_state_mean = as.data.frame(cbind(combine_stats[match(rownames(feature_state_mean),combine_stats$State),]$Samples_avg_all,feature_state_mean))
-colnames(feature_state_mean)[1] = "All"
+feature_state_mean = melt(as.matrix(feature_state_mean))
+colnames(feature_state_mean) = c("State","Feature","Enrichment")
+feature_state_mean$Feature = factor(feature_state_mean$Feature,levels=levels(feature_state_mean$Feature)[c(17,19,18,5,7,6,8:9,2,4,3,10,12,11,14,16,15,13,1,20)])
+
+# By class
+feature_state_mean_class = ddply(rmsk_TE_measure,~class_update,function(z) 
+  apply(z[,15:34],2,function(y) 
+    (colMeans(z[which(y == "yes"),c(35:49,52:55,60:62)],na.rm=TRUE)-colMeans(z[which(y == "no"),c(35:49,52:55,60:62)],na.rm=TRUE))/colMeans(z[,c(35:49,52:55,60:62)],na.rm=TRUE)))
+feature_state_mean_class$State = factor(rep(c(chromHMM_states,colnames(rmsk_TE_measure)[c(52:55,60:62)]),6),levels=c(chromHMM_states,meth_states,"DNase","H3K27ac","Expressed_samples"))
+
+feature_state_mean_class[which(feature_state_mean_class$State %in% chromHMM_states),2:21] = feature_state_mean_class[which(feature_state_mean_class$State %in% chromHMM_states),2:21]/1.27
+feature_state_mean_class[which(feature_state_mean_class$State %in% meth_states),2:21] = feature_state_mean_class[which(feature_state_mean_class$State %in% meth_states),2:21]/0.37
+feature_state_mean_class[which(feature_state_mean_class$State == "DNase"),2:21] = feature_state_mean_class[which(feature_state_mean_class$State == "DNase"),2:21]/0.53
+feature_state_mean_class[which(feature_state_mean_class$State == "H3K27ac"),2:21] = feature_state_mean_class[which(feature_state_mean_class$State == "H3K27ac"),2:21]/0.98
+feature_state_mean_class[which(feature_state_mean_class$State == "Expressed_samples"),2:21] = feature_state_mean_class[which(feature_state_mean_class$State == "Expressed_samples"),2:21]/0.52
+
+feature_state_mean_class = melt(feature_state_mean_class)
+colnames(feature_state_mean_class) = c("Class","State","Feature","Enrichment")
+feature_state_mean_class$Feature = factor(feature_state_mean_class$Feature,levels=levels(feature_state_mean_class$Feature)[c(17,19,18,5,7,6,8:9,2,4,3,10,12,11,14,16,15,13,1,20)])
