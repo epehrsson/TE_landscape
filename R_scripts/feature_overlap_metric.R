052e714b77e94e16b619249dd872d9288ec8a69a
Erica Pehrsson <epehrsson@wustl.edu>
2017-09-22 18:14:23 -0500

    Plotting TE metrics by feature overlap

---
+++ R_scripts/feature_overlap_metric.R
@@ -0,0 +1,24 @@
+# Average TE metrics by Refseq feature overlap
+source("R_scripts/TE_correlation.R")
+
+# For TEs overlapping each feature, metric average
+feature_metric_mean = melt(as.matrix(apply(rmsk_TE_measure[,15:34],2,function(y) 
+  (colMeans(rmsk_TE_measure[which(y == "yes"),c(8,10,12:14)],na.rm=TRUE)-colMeans(rmsk_TE_measure[which(y == "no"),c(8,10,12:14)],na.rm=TRUE))/colMeans(rmsk_TE_measure[,c(8,10,12:14)],na.rm=TRUE))))
+
+colnames(feature_metric_mean) = c("Metric","Feature","Enrichment")
+feature_metric_mean$Feature = factor(feature_metric_mean$Feature,levels=levels(feature_metric_mean$Feature)[c(17,19,18,5,7,6,8:9,2,4,3,10,12,11,14,16,15,13,1,20)])
+
+# By class
+feature_metric_mean_class = ddply(rmsk_TE_measure,~class_update,function(z) 
+  apply(z[,15:34],2,function(y) 
+    (colMeans(z[which(y == "yes"),c(8,10,12:14)],na.rm=TRUE)-colMeans(z[which(y == "no"),c(8,10,12:14)],na.rm=TRUE))/colMeans(z[,c(8,10,12:14)],na.rm=TRUE)))
+feature_metric_mean_class$Metric = rep(colnames(rmsk_TE_measure)[c(8,10,12:14)],6)
+feature_metric_mean_class = melt(feature_metric_mean_class,id.vars=c("class_update","Metric"))
+colnames(feature_metric_mean_class) = c("Class","Metric","Feature","Enrichment")
+feature_metric_mean_class$Feature = factor(feature_metric_mean_class$Feature,levels=levels(feature_metric_mean_class$Feature)[c(17,19,18,5,7,6,8:9,2,4,3,10,12,11,14,16,15,13,1,20)])
+
+# Older code
+#test = melt(as.matrix(apply(rmsk_TE_measure[,15:34],2,function(x) apply(rmsk_TE_measure[,c(8,10,12:14)],2,function(y) {table = aggregate(data=rmsk_TE_measure,y~x,mean);(table$y[2]-table$y[1])/mean(y)}))))
+
+#compare_TE_feature = ddply(rmsk_TE_measure,~class_update,function(z) apply(z[,15:34],2,function(x) apply(z[,c(8,10,12:14)],2,function(y) {table = aggregate(data=z,y~x,mean);(table$y[2]-table$y[1])/mean(y)})))
+#compare_TE_feature$Metric = rep(colnames(rmsk_TE_measure)[c(8,10,12:14)],6)
