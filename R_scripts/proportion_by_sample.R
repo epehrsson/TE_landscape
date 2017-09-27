93549e82b631fb1996f586675bab3b4e4ff05f21
Erica Pehrsson <epehrsson@wustl.edu>
2017-09-26 22:50:36 -0500

    Updated to match proportion.R, include all methylation states

--- R_scripts/proportion_by_sample.R
+++ R_scripts/proportion_by_sample.R
@@ -4,14 +4,13 @@ source("R_scripts/proportion.R")
 
 # chromHMM
 # Proportion of each chromHMM state in TEs by sample
-mnemonics_states_TE_proportion = melt(as.matrix(mnemonics_states_TE/mnemonics_states_genome))
+mnemonics_states_TE_proportion = melt(as.matrix(mnemonics_states_TE[2:16,1:127]/mnemonics_states_genome[2:16,1:127]))
 colnames(mnemonics_states_TE_proportion) = c("State","Sample","Proportion")
 
 # WGBS
-# Proportion of hypomethylated CpGs in TEs by sample (see WGBS_sample_TE_state.R)
-WGBS_proportion = as.data.frame(cbind(rownames(TE_CpG_meth),TE_CpG_meth$Hypomethylated/all_CpG_meth$Hypomethylated))
-colnames(WGBS_proportion) = c("Sample","Proportion")
-WGBS_proportion$State = rep("Hypomethylated",37)
+# Proportion of hypomethylated CpGs in TEs by sample
+WGBS_proportion = melt(as.matrix(TE_CpG_meth/all_CpG_meth))
+colnames(WGBS_proportion) = c("Sample","State","Proportion")
 
 # DNase
 # Proportion of Dnase peaks overlapping TEs by sample classification
