266b212a34bb08fc10dda8f9b43be3e07e0dd69a
Erica Pehrsson <epehrsson@wustl.edu>
2017-10-09 11:44:24 -0500

    Updated with coding split

--- R_scripts/feature_overlap_metric.R
+++ R_scripts/feature_overlap_metric.R
@@ -1,12 +1,13 @@
 # Average TE metrics by Refseq feature overlap
+
 source("R_scripts/TE_correlation.R")
 
 # For TEs overlapping each feature, metric average
 feature_metric_mean = melt(as.matrix(apply(rmsk_TE_measure[,15:34],2,function(y) 
   (colMeans(rmsk_TE_measure[which(y == "yes"),c(8,10,12:14)],na.rm=TRUE)-colMeans(rmsk_TE_measure[which(y == "no"),c(8,10,12:14)],na.rm=TRUE))/colMeans(rmsk_TE_measure[,c(8,10,12:14)],na.rm=TRUE))))
 
-colnames(feature_metric_mean) = c("Metric","Feature","Enrichment")
-feature_metric_mean$Feature = factor(feature_metric_mean$Feature,levels=levels(feature_metric_mean$Feature)[c(17,19,18,5,7,6,8:9,2,4,3,10,12,11,14,16,15,13,1,20)])
+colnames(feature_metric_mean) = c("Metric","Cohort","Enrichment")
+feature_metric_mean = split_coding(feature_metric_mean,2)
 
 # By class
 feature_metric_mean_class = ddply(rmsk_TE_measure,~class_update,function(z) 
@@ -14,8 +15,8 @@ feature_metric_mean_class = ddply(rmsk_TE_measure,~class_update,function(z)
     (colMeans(z[which(y == "yes"),c(8,10,12:14)],na.rm=TRUE)-colMeans(z[which(y == "no"),c(8,10,12:14)],na.rm=TRUE))/colMeans(z[,c(8,10,12:14)],na.rm=TRUE)))
 feature_metric_mean_class$Metric = rep(colnames(rmsk_TE_measure)[c(8,10,12:14)],6)
 feature_metric_mean_class = melt(feature_metric_mean_class,id.vars=c("class_update","Metric"))
-colnames(feature_metric_mean_class) = c("Class","Metric","Feature","Enrichment")
-feature_metric_mean_class$Feature = factor(feature_metric_mean_class$Feature,levels=levels(feature_metric_mean_class$Feature)[c(17,19,18,5,7,6,8:9,2,4,3,10,12,11,14,16,15,13,1,20)])
+colnames(feature_metric_mean_class) = c("Class","Metric","Cohort","Enrichment")
+feature_metric_mean_class = split_coding(feature_metric_mean_class,3)
 
 # Older code
 #test = melt(as.matrix(apply(rmsk_TE_measure[,15:34],2,function(x) apply(rmsk_TE_measure[,c(8,10,12:14)],2,function(y) {table = aggregate(data=rmsk_TE_measure,y~x,mean);(table$y[2]-table$y[1])/mean(y)}))))
