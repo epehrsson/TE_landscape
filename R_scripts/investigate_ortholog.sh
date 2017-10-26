9719436910fe5f54e1d21f098ed0f333e4939314
Erica Pehrsson <epehrsson@wustl.edu>
2017-10-26 10:17:38 -0500

    Updated with more recent GENCODE overlap code

--- R_scripts/investigate_ortholog.sh
+++ R_scripts/investigate_ortholog.sh
@@ -1,5 +1,5 @@
 # Mouse-human candidates
-# 5/30/2017, 7/26/2017, 7/31/2017
+# 5/30/2017, 7/26/2017, 7/31/2017, 10/3/2017
 
 # hg19 TEs hypomethlated in human/mouse also in promoter state	
 #TE_landscape/Mouse/human_mouse_hypo_1TssA.bed	
@@ -8,3 +8,7 @@ write.table(unique(human_mouse_orthologs_mm10_hypo[which(human_mouse_orthologs_m
 # Overlap with GENCODE promoters	
 #TE_landscape/Mouse/human_mouse_hypo_1TssA_Gencode.txt	
 bedtools intersect -wo -a human_mouse_hypo_1TssA.bed -b ~/genic_features/Gencodev19_promoter.txt > human_mouse_hypo_1TssA_Gencode.txt
+
+# Update 10/3/2017
+## Overlap with GENCODE promoters 
+bedtools intersect -wo -a hg19_orthologs_hypo_1TssA.bed -b ~/genic_features/Gencode/Gencodev19_promoter.txt > hg19_orthologs_hypo_1TssA_Gencode.txt
