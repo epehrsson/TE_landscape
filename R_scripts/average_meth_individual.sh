252638320f82d4387d034869c1e120ee7fd7fa5b
Erica Pehrsson <epehrsson@wustl.edu>
2017-10-26 10:17:20 -0500

    Updated with TEs overlapping a CpG in each state

--- R_scripts/average_meth_individual.sh
+++ R_scripts/average_meth_individual.sh
@@ -1,5 +1,5 @@
 # Average methylation level over individual feature
-# 5/9/2016, 12/14/16, 2/2/2017, 5/11/2017, 6/19/2017, 8/7/2017, 8/23/2017, 8/28/2017
+# 5/9/2016, 12/14/16, 2/2/2017, 5/11/2017, 6/19/2017, 8/7/2017, 8/23/2017, 8/28/2017, 9/29/2017
 
 # Average methylation level of each TE in each sample	 
 #TE_landscape/WGBS/TE_CpG_Meth_new_average.txt	
@@ -10,6 +10,12 @@ python ../bin/TE_landscape/average_methylation.py TE_CpG_Meth.bed rmsk_TE.txt WG
 #TE_landscape/WGBS/methylation_old/other_CpG_Meth_avg.bed		 
 python ../bin/TE_landscape/average_methylation.py other_CpG_Meth.bed rmsk_other.txt WGBS_samples.txt other_CpG_Meth_avg.bed
 #TE_landscape/WGBS/methylation_old/error/TE_CpG_Meth_avg.bed		
+
+# TEs overlapping a CpG in each methylation state by sample
+ python ~/bin/TE_landscape/count_CpG_state.py TE_CpG_Meth_new.bed TE_CpG_count.txt ../sample_lists/WGBS_samples.txt TE_CpG_Meth_state.txt
+
+# Count by subfamily
+ tail -n +2 TE_CpG_Meth_state.txt | awk -v OFS='\t' '{a[$4,$8]+=1; if($12 > 0) miss[$4,$8]+=1; if($9 > 0) hypo[$4,$8]+=1; if($11 > 0) hyper[$4,$8]+=1; if($10 > 0) inter[$4,$8]+=1;} END{for (i in a){split (i, sep, SUBSEP); print sep[1], sep[2], hypo[i], inter[i], hyper[i], miss[i];}}' - > subfamily_CpG_state_members.txt
 
 # Average methylation of unique Refseq promoters	 
 #TE_landscape/WGBS/Refseq_promoters/refseq_promoter_unique_CpG_Meth_average.txt	
