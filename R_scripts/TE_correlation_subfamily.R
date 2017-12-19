492c72b0480ca54b94c80c855d2c531011920dca
Erica Pehrsson <epehrsson@wustl.edu>
2017-12-19 17:49:16 -0600

    Removed mean, max, range; RNA expression

--- R_scripts/TE_correlation_subfamily.R
+++ R_scripts/TE_correlation_subfamily.R
@@ -13,29 +13,6 @@ colnames(test)[2:22] = lapply(colnames(test)[2:22],function(x) paste(x,"PC",sep=
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,test,by="subfamily",all.x=TRUE)
 rm(test)
 
-# chromHMM proportion mean, max, range per subfamily
-test = melt(ddply(subfamily_state_sample,~subfamily+State,summarise,Max = max(2^Enrichment),Mean = mean(2^Enrichment),Range = (max(2^Enrichment)-min(2^Enrichment))),id.vars=c("subfamily","State"))
-test$Header = apply(test,1,function(x) paste(x[2],x[3],sep="_"))
-test = dcast(test[,c(1,4:5)],subfamily~Header)
-rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,test,by="subfamily")
-rm(test)
-
-# Methylation state proportion mean, max, range per subfamily
-test = melt(ddply(subfamily_CpG_meth,~subfamily+State,summarise,Max = max(2^Enrichment),Mean = mean(2^Enrichment),Range = (max(2^Enrichment)-min(2^Enrichment))),id.vars=c("subfamily","State"))
-test$Header = apply(test,1,function(x) paste(x[2],x[3],sep="_"))
-test = dcast(test[,c(1,4:5)],subfamily~Header)
-rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,test,by="subfamily")
-rm(test)
-
-# Add DNase proportion mean, max, range
-rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,ddply(subfamily_DNase_sample,~subfamily,summarise,DNase_Max = max(2^Enrichment),DNase_Mean = mean(2^Enrichment),DNase_Range = (max(2^Enrichment)-min(2^Enrichment))),by="subfamily")
-
-# Add H3K27ac proportion mean, max, range
-rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,ddply(subfamily_H3K27ac_sample,~subfamily,summarise,H3K27ac_Max = max(2^Enrichment),H3K27ac_Mean = mean(2^Enrichment),H3K27ac_Range = (max(2^Enrichment)-min(2^Enrichment))),by="subfamily")
-
-# Adding RNA expression 
-rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,RNA_TE_agnostic_subfamily[,c(3,57:58)],by="subfamily")
-
 # Adding C-GATE
 rmsk_TE_subfamily_measure$CGate = rep("no",968)
 rmsk_TE_subfamily_measure[which(rmsk_TE_subfamily_measure$subfamily %in% cgate_subfams),]$CGate = "yes"
