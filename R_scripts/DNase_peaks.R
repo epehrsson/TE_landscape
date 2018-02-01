6fe2ca1e782e3d0203c224a1648ceedc3f8c0fe5
Erica Pehrsson <epehrsson@wustl.edu>
2018-02-01 15:45:34 -0600

    Creating length of overlap matrices, added class factor function

--- R_scripts/DNase_peaks.R
+++ R_scripts/DNase_peaks.R
@@ -1,18 +1,18 @@
 # Load matrix of TE x sample x DNase peak overlap
 # See 5/24/2017, 6/5/2017
 
-library(reshape2)
-
-# Number of overlaps between TEs, DNase peaks
 TE_DNase_peaks = read.table(file="DNase/rmsk_TEother_DNase_peaks_filter.txt",sep='\t')
 colnames(TE_DNase_peaks) = c("chromosome","start","stop","subfamily","class","family","strand","Sample","Peaks","Overlap")
+
+# Length of overlaps between TEs, DNase peaks
+TE_DNase_overlap = dcast(TE_DNase_peaks,chromosome+start+stop+subfamily+family+class+strand~Sample,value.var="Overlap")
+TE_DNase_overlap[is.na(TE_DNase_overlap)] = 0
+TE_DNase_overlap$class_update = convert_class(TE_DNase_overlap$class)
+
+# Number of overlaps between TEs, DNase peaks
 TE_DNase_peaks = dcast(TE_DNase_peaks,chromosome+start+stop+subfamily+family+class+strand~Sample,value.var="Peaks")
 TE_DNase_peaks[is.na(TE_DNase_peaks)] = 0
-
-TE_DNase_peaks$class_update = TE_DNase_peaks$class
-TE_DNase_peaks$class_update = factor(TE_DNase_peaks$class_update,levels=c("DNA","LINE","LTR","SINE","SVA","Other"))
-TE_DNase_peaks[which(TE_DNase_peaks$class == "Other"),]$class_update = "SVA"
-TE_DNase_peaks[which(TE_DNase_peaks$class %in% c("DNA?","LINE?","LTR?","SINE?","Unknown","Unknown?","RC")),]$class_update = "Other"
+TE_DNase_peaks$class_update = convert_class(TE_DNase_peaks$class)
 
 # Number of samples a TE overlaps a DNase peak
 TE_DNase_peaks$Samples = apply(TE_DNase_peaks,1,function(x) sum(as.numeric(x[8:60]) > 0))
@@ -20,4 +20,4 @@ TE_DNase_peaks$Samples = apply(TE_DNase_peaks,1,function(x) sum(as.numeric(x[8:6
 # Number of samples a TE overlaps a DNase peak, no cancer cell lines
 TE_DNase_peaks$Samples_noCancer = apply(TE_DNase_peaks[,8:60],1,function(x) sum(as.numeric(x[which(metadata[match(colnames(TE_DNase_peaks)[8:60],metadata$Sample),]$Exclude == "Include")]) > 0))
 
-save(TE_DNase_peaks,file="R_datasets/TE_DNase_peaks.RData")
+save(list=c("TE_DNase_peaks","TE_DNase_overlap"),file="R_datasets/TE_DNase_peaks.RData")
--- R_scripts/H3K27ac_peaks.R
+++ R_scripts/H3K27ac_peaks.R
@@ -1,18 +1,18 @@
 # Load matrix of TE x sample x H3K27ac peak overlap
 # See 7/4/2017
 
-library(reshape2)
-
-# Number of overlaps between TEs, H3K27ac peaks
 TE_H3K27ac_peaks = read.table(file="H3K27ac/rmsk_TEother_H3K27ac_peaks_filter.txt",sep='\t')
 colnames(TE_H3K27ac_peaks) = c("chromosome","start","stop","subfamily","class","family","strand","Sample","Peaks","Overlap")
+
+# Length of overlaps between TEs, H3K27ac peaks
+TE_H3K27ac_overlap = dcast(TE_H3K27ac_peaks,chromosome+start+stop+subfamily+family+class+strand~Sample,value.var="Overlap")
+TE_H3K27ac_overlap[is.na(TE_H3K27ac_overlap)] = 0
+TE_H3K27ac_overlap$class_update = convert_class(TE_H3K27ac_overlap$class)
+
+# Number of overlaps between TEs, H3K27ac peaks
 TE_H3K27ac_peaks = dcast(TE_H3K27ac_peaks,chromosome+start+stop+subfamily+family+class+strand~Sample,value.var="Peaks")
 TE_H3K27ac_peaks[is.na(TE_H3K27ac_peaks)] = 0
-
-TE_H3K27ac_peaks$class_update = TE_H3K27ac_peaks$class
-TE_H3K27ac_peaks$class_update = factor(TE_H3K27ac_peaks$class_update,levels=c("DNA","LINE","LTR","SINE","SVA","Other"))
-TE_H3K27ac_peaks[which(TE_H3K27ac_peaks$class == "Other"),]$class_update = "SVA"
-TE_H3K27ac_peaks[which(TE_H3K27ac_peaks$class %in% c("DNA?","LINE?","LTR?","SINE?","Unknown","Unknown?","RC")),]$class_update = "Other"
+TE_H3K27ac_peaks$class_update = convert_class(TE_H3K27ac_peaks$class)
 
 # Number of samples a TE overlaps a H3K27ac peak
 TE_H3K27ac_peaks$Samples = apply(TE_H3K27ac_peaks,1,function(x) sum(as.numeric(x[8:105]) > 0))
@@ -20,4 +20,4 @@ TE_H3K27ac_peaks$Samples = apply(TE_H3K27ac_peaks,1,function(x) sum(as.numeric(x
 # Number of samples a TE overlaps a H3K27ac peak, no cancer cell lines
 TE_H3K27ac_peaks$Samples_noCancer = apply(TE_H3K27ac_peaks[,8:105],1,function(x) sum(as.numeric(x[which(metadata[match(colnames(TE_H3K27ac_peaks)[8:105],metadata$Sample),]$Exclude == "Include")]) > 0))
 
-save(TE_H3K27ac_peaks,file="R_datasets/TE_H3K27ac_peaks.RData")
+save(list=c("TE_H3K27ac_peaks","TE_H3K27ac_overlap"),file="R_datasets/TE_H3K27ac_peaks.RData")
