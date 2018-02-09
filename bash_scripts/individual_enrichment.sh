2b6e681b6e640d3e322e1a31fea99c30e819eba9
Erica Pehrsson <epehrsson@wustl.edu>
2018-02-09 15:37:16 -0600

    Combined individual_enrichment scripts, updated with new separate subfamily/state files, updated run rile

--- bash_scripts/individual_enrichment.sh
+++ bash_scripts/individual_enrichment.sh
@@ -3,19 +3,19 @@
 # 9/30/2016, 5/4/2017, 5/24/2017, 5/29/2017, 5/30/2017, 5/31/2017
 
 input_matrix=$1 #Three columns: subfamily, sample, state
-combined_output=$2
+metric=$2
 
-# Filter subfamily-state to subfamily-sample-state trios 
+if [ $metric -eq "chromHMM" ]; then
+   split_path=chromHMM/subfamily/by_state/
+elif [ $metric -eq "WGBS" ]; then
+   split_path=WGBS/subfamily/by_state/
+fi
+
+# Filter subfamily-state to subfamily-sample-state trios, reformat to bedfile of unique TEs in state when enriched
 while read subfamily sample state
 do
-  awk -v OFS='\t' -v sample=$sample '{if($10 == sample) print $0}' chromHMM/subfamily/by_state/$subfamily\_$state\.txt >> $combined_output
+  if [ $state -eq "8_ZNF/Rpts" ]; then
+    $state="8_ZNF.Rpts"
+  fi
+  awk -v OFS='\t' -v sample=$sample '{if($10 == sample) print $0}' $split_path$subfamily\_$state\.txt | awk -v OFS='\t' '{a[$1, $2, $3, $4, $5, $6, $7]+=1}END{for(i in a){split(i,sep,SUBSEP); print sep[1], sep[2], sep[3], sep[4], a[i], sep[7];}}' - > enrichment/bedfiles/$subfamily\_$state\_enriched.bed
 done < $input_matrix
-
-# Split by subfamily, reformat to bed file, unique TEs in state when enriched
-while read subfamily state
-do
-  awk -v OFS='\t' -v subfam=$subfamily -v state=$state '{if(($4 == subfam) && ($8 == state)) a[$1, $2, $3, $4, $5, $6, $7]+=1}END{for(i in a){split(i,sep,SUBSEP); print sep[1], sep[2], sep[3], sep[4], a[i], sep[7];}}' $combined_output > enrichment/$subfamily\_$state\_enriched.bed
-done < <(awk -v OFS='\t' '{print $1, $3}' $input_matrix | sort | uniq)
-
-# For 8_ZNF/Rpts
-#  while read subfamily state; do awk -v OFS='\t' -v subfam=$subfamily '{if($4 == subfam) a[$1, $2, $3, $4, $5, $6, $7]+=1}END{for(i in a){split(i,sep,SUBSEP); print sep[1], sep[2], sep[3], sep[4], a[i], sep[7];}}' enrichment/candidate_8_ZNF.Rpts_chromHMM.txt > enrichment/$subfamily\_8_ZNF.Rpts_enriched.bed; done < <(awk -v OFS='\t' '{print $1, $3}' enrichment/candidate_8_ZNF.Rpts_enrich.txt | sort | uniq)
