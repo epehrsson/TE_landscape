a19bce18200e8739ff8d4e48c4c0d90dce21d7b5
Erica Pehrsson <epehrsson@wustl.edu>
2018-02-15 09:00:59 -0600

    Moved directory, changed to python script from bash script

--- bash_scripts/individual_enrichment.sh
+++ bash_scripts/individual_enrichment.sh
@@ -17,5 +17,6 @@ do
   if [ $state = "8_ZNF/Rpts" ]; then
     $state="8_ZNF.Rpts"
   fi
+  # Change so WGBS column is correct, all subfamily x sample processed at the same time into bedfile
   awk -v OFS='\t' -v sample=$sample '{if($10 == sample) print $0}' "$split_path"$subfamily\_$state\.txt | awk -v OFS='\t' '{a[$1, $2, $3, $4, $5, $6, $7]+=1}END{for(i in a){split(i,sep,SUBSEP); print sep[1], sep[2], sep[3], sep[4], a[i], sep[7];}}' - > enrichment/bedfiles/$subfamily\_$state\_enriched.bed
 done < $input_matrix
