aecc09dacd9cbd30dd48f5331cb99ea4e98019b8
Erica Pehrsson <epehrsson@wustl.edu>
2018-02-28 13:18:18 -0600

    Refactored code, included no chrY feature length. Now no manual editing required.

--- bash_scripts/feature_overlap_length.sh
+++ bash_scripts/feature_overlap_length.sh
@@ -1,24 +1,27 @@
 # Feature overlap
 # 6/13/2017, 6/14/2017, 6/15/2017
 
-#TE_landscape/features/intersect_features/feature_overlap.txt	
-# Length of overlap between TEs, Refseq features	 
-for file in ~/genic_features/refseq*merge*txt; do echo $file; awk '{if($1 !~ /_/) sum+=$3-$2}END{print sum}' $file; done
-for file in ~/genic_features/refseq*merge*txt; do bedtools intersect -wo -a rmsk_TEother_merge.txt -b $file | awk '{sum+=$7}END{print sum}' -; done
-awk '{if($1 !~ /_/) sum+=$3-$2}END{print sum}' ~/genic_features/refseq_intergenic.txt
-bedtools intersect -wo -a rmsk_TEother_merge.txt -b ~/genic_features/refseq_intergenic.txt | awk '{sum+=$7}END{print sum}' -
-
-# Length of stranded overlap	 
-for file in ~/genic_features/refseq_*.txt; do echo $file; awk -v OFS='\t' '{print $1, $2, $3, $4, '0', $7}' rmsk_TEother.txt | bedtools intersect -a - -b $file -s | sort -k1,1V -k2,2n - | bedtools merge -i - | awk '{sum+=$3-$2}END{print sum}'; done
-
-# Length of coding/non-coding features	 
-for file in refseq*pc_merge.txt; do echo $file ; awk '{if($1 !~ /_/) sum+=$3-$2}END{print sum}' $file ; done
-for file in refseq*nc_merge.txt; do echo $file ; awk '{if($1 !~ /_/) sum+=$3-$2}END{print sum}' $file ; done
-
-# Length of overlap between TEs, Refseq features (coding/non-coding)	 
-for file in ~/genic_features/refseq*pc_merge.txt; do echo $file; bedtools intersect -wo -a rmsk_TEother_merge.txt -b $file | awk '{sum+=$7}END{print sum}' -; done
-for file in ~/genic_features/refseq*nc_merge.txt; do echo $file; bedtools intersect -wo -a rmsk_TEother_merge.txt -b $file | awk '{sum+=$7}END{print sum}' -; done
-
-# Length of stranded overlap between TEs, Refseq features (coding/non-coding)	 
-for file in ~/genic_features/refseq_*_pc.txt; do echo $file; awk -v OFS='\t' '{print $1, $2, $3, $4, '0', $7}' rmsk_TEother.txt | bedtools intersect -a - -b $file -s | sort -k1,1V -k2,2n - | bedtools merge -i - | awk '{sum+=$3-$2}END{print sum}'; done
-for file in ~/genic_features/refseq_*_nc.txt; do echo $file; awk -v OFS='\t' '{print $1, $2, $3, $4, '0', $7}' rmsk_TEother.txt | bedtools intersect -a - -b $file -s | sort -k1,1V -k2,2n - | bedtools merge -i - | awk '{sum+=$3-$2}END{print sum}'; done
+#TE_landscape/features/intersect_features/feature_overlap.txt
+while read feature
+do 
+  if [ $feature = "intergenic" ]; then
+    echo "refseq_intergenic"
+    awk '{if($1 !~ /_/) sum+=$3-$2}END{print sum}' ~/genic_features/RefSeq/refseq_intergenic.txt
+    awk '{if(($1 !~ /_/) && ($1 != "chrY")) sum+=$3-$2}END{print sum}' ~/genic_features/RefSeq/refseq_intergenic.txt
+    bedtools intersect -wo -a features/TEs/merge/rmsk_TEother_merge.txt -b ~/genic_features/RefSeq/refseq_intergenic.txt | awk '{sum+=$7}END{print sum}' -   
+    bedtools intersect -wo -a features/TEs/merge/rmsk_TEother_merge.txt -b ~/genic_features/RefSeq/refseq_intergenic.txt | awk '{sum+=$7}END{print sum}' -
+  else
+    for file in ~/genic_features/RefSeq/refseq_$feature*_merge.txt
+    do
+      echo $( basename $file _merge.txt )
+      # Length of feature
+      awk '{if($1 !~ /_/) sum+=$3-$2}END{print sum}' $file
+      # Length of feature, no chrY
+      awk '{if(($1 !~ /_/) && ($1 != "chrY")) sum+=$3-$2}END{print sum}' $file
+      # Length of overlap between TEs, Refseq features, unstranded  
+      bedtools intersect -wo -a features/TEs/merge/rmsk_TEother_merge.txt -b $file | awk '{sum+=$7}END{print sum}' -
+      # Length of overlap between TEs, Refseq features, stranded
+      awk -v OFS='\t' '{print $1, $2, $3, $4, '0', $7}' features/TEs/rmsk_TEother.txt | bedtools intersect -a - -b ~/genic_features/RefSeq/$( basename $file _merge.txt ).txt -s | sort -k1,1V -k2,2n - | bedtools merge -i - | awk '{sum+=$3-$2}END{print sum}'
+    done
+  fi
+done < features/features.txt > features/intersect_features/feature_overlap.txt
