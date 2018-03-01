1579c0203e0e0fd991517e8590fe93c89a785d10
Erica Pehrsson <epehrsson@wustl.edu>
2018-03-01 15:20:18 -0600

    ChrY no longer hardcoded; DNase/H3K27ac width from features, not chromHMM

--- R_scripts/subfamily_enrichment.R
+++ R_scripts/subfamily_enrichment.R
@@ -2,13 +2,6 @@
 # See 4/27/2016, 4/28/2016, 5/4/2016, 5/24/2016, 6/29/2016, 7/6/2016, 7/7/2016, 7/8/2016, 7/10/2016, 7/11/2016, 8/28/2016, 8/29/2016, 9/8/2016, 9/9/2016, 9/17/2016, 9/18/2016, 9/27/2016, 9/29/2016, 11/9/2016, 11/10/2016, 11/15/2016, 11/16/2016, 11/22/2016, 11/27/2016, 
 # 1/13/2017, 1/19/2017, 1/20/2017, 1/26/2017, 2/6/2017, 2/9/2017, 2/10/2017, 2/13/2017, 2/14/2017, 2/16/2017, 2/21/2017, 3/16/2017, 5/15/2017, 5/16/2017, 5/23/2017, 5/24/2017, 5/29/2017, 5/30/2017, 6/5/17, 6/6/17, 6/7/17, 6/9/17, 6/15/2017, 7/4/2017, 7/21/2017, 7/23/2017, 7/24/2017, 7/26/2017, 8/2/2017
 
-#source("R_scripts/TE_subfamily_stats.R")
-#source("R_scripts/WGBS_sample_CpG_state.R")
-#source("R_scripts/DNase_overlap.R")
-#source("R_scripts/H3K27ac_overlap.R")
-#load("R_datasets/TE_DNase_peaks.RData")
-#load("R_datasets/TE_H3K27ac_peaks.RData")
-
 # chromHMM
 # Number of bp in each subfamily x sample x state
 subfamily_state_sample = read.table("chromHMM/subfamily/subfamily_state_sample.txt",sep='\t')
@@ -54,9 +47,9 @@ subfamily_state_sample_members = rbind(read.table("chromHMM/subfamily/subfamily_
 colnames(subfamily_state_sample_members) = c("subfamily","class","family","State","Sample","Members")
 
 # Proportion of members in state
-subfamily_state_sample_members$Percent = subfamily_state_sample_members$Members/rmsk_TE_subfamily[match(subfamily_state_sample_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count
-subfamily_state_sample_members[which(subfamily_state_sample_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Percent = subfamily_state_sample_members[which(subfamily_state_sample_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Members/rmsk_TE_subfamily[match(subfamily_state_sample_members[which(subfamily_state_sample_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY
-
+subfamily_state_sample_members$Percent = subfamily_state_sample_members$Members/ifelse(metadata[match(subfamily_state_sample_members$Sample,metadata$Sample),]$chrY == "Yes",
+                                                                                       rmsk_TE_subfamily[match(subfamily_state_sample_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count,
+                                                                                       rmsk_TE_subfamily[match(subfamily_state_sample_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY)
 subfamily_state_sample = merge(subfamily_state_sample,subfamily_state_sample_members,by=c("subfamily","family","State","Sample"),all.x=TRUE)
 subfamily_state_sample[which(is.na(subfamily_state_sample$Members)),]$Members = 0
 subfamily_state_sample[which(is.na(subfamily_state_sample$Percent)),]$Percent = 0
@@ -115,7 +108,9 @@ subfamily_DNase_sample_expand = expand.grid(subfamily = levels(rmsk_TE_subfamily
 subfamily_DNase_sample_expand = join(subfamily_DNase_sample_expand,rmsk_TE_subfamily[,c("subfamily","family","class_update","Total_length")],by=c("subfamily"))
 subfamily_DNase_sample = join(subfamily_DNase_sample_expand,subfamily_DNase_sample,by=c("subfamily","Sample"),type="left")
 colnames(subfamily_DNase_sample)[5] = "Length_ik"
-subfamily_DNase_sample[which(metadata[match(subfamily_DNase_sample$Sample,metadata$Sample),]$chrY == "No"),]$Length_ik = apply(subfamily_DNase_sample[which(metadata[match(subfamily_DNase_sample$Sample,metadata$Sample),]$chrY == "No"),],1,function(x) rmsk_TE_subfamily[match(x[1],rmsk_TE_subfamily$subfamily),]$Total_length_noY)
+subfamily_DNase_sample$Length_ik = ifelse(metadata[match(subfamily_DNase_sample$Sample,metadata$Sample),]$chrY == "Yes",
+                                          subfamily_DNase_sample$Length_ik,
+                                          rmsk_TE_subfamily[match(subfamily_DNase_sample$subfamily,rmsk_TE_subfamily$subfamily),]$Total_length_noY)
 subfamily_DNase_sample[is.na(subfamily_DNase_sample$Length_ijk),]$Length_ijk = 0
 rm(subfamily_DNase_sample_expand)
 
@@ -124,10 +119,7 @@ subfamily_DNase_sample = merge(subfamily_DNase_sample,DNase_stats[,c(1,4)],by=c(
 colnames(subfamily_DNase_sample)[7] = "Length_jk"
 
 # Total length of sample
-mnemonics_states_genome = melt(as.matrix(t(read.table("chromHMM/genome/mnemonics_state.txt",sep='\t',header=TRUE,row.names=1)[1,])))[,c(1,3)]
-colnames(mnemonics_states_genome) = c("Sample","Length_k")
-subfamily_DNase_sample = join(subfamily_DNase_sample,mnemonics_states_genome,by=c("Sample"),type="left")
-rm(mnemonics_states_genome)
+subfamily_DNase_sample$Length_k = ifelse(metadata[match(subfamily_DNase_sample$Sample,metadata$Sample),]$chrY == "Yes",GENOME_WIDTH,GENOME_WIDTH_noY)
 
 # LOR enrichment
 subfamily_DNase_sample$Enrichment = log2((subfamily_DNase_sample$Length_ijk/subfamily_DNase_sample$Length_ik)/(subfamily_DNase_sample$Length_jk/subfamily_DNase_sample$Length_k))
@@ -141,8 +133,9 @@ subfamily_DNase_sample = merge(subfamily_DNase_sample,metadata[,c(1,4:9)],by=c("
 # Number of members overlapping DNase peak
 TE_DNase_peaks_members = merge(melt(aggregate(data=TE_DNase_peaks[,c(4,8:60)],.~subfamily,function(x) sum(as.numeric(x))),id.vars=c("subfamily")),melt(aggregate(data=TE_DNase_peaks[,c(4,8:60)],.~subfamily,function(x) sum(as.numeric(x) > 0)),id.vars=c("subfamily")),by=c("subfamily","variable"))
 colnames(TE_DNase_peaks_members)[2:4] = c("Sample","DNase_peaks","Members")
-TE_DNase_peaks_members = merge(TE_DNase_peaks_members,rmsk_TE_subfamily[,c("subfamily","Count")],by="subfamily")
-TE_DNase_peaks_members[which(TE_DNase_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Count = rmsk_TE_subfamily[match(TE_DNase_peaks_members[which(TE_DNase_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY
+TE_DNase_peaks_members$Count = ifelse(metadata[match(TE_DNase_peaks_members$Sample,metadata$Sample),]$chrY == "Yes",
+                                      rmsk_TE_subfamily[match(TE_DNase_peaks_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count,
+                                      rmsk_TE_subfamily[match(TE_DNase_peaks_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY)
 
 # Proportion of members overlapping DNase peak
 TE_DNase_peaks_members$Percent = TE_DNase_peaks_members$Members/TE_DNase_peaks_members$Count
@@ -166,7 +159,9 @@ subfamily_H3K27ac_sample_expand = expand.grid(subfamily = levels(rmsk_TE_subfami
 subfamily_H3K27ac_sample_expand = join(subfamily_H3K27ac_sample_expand,rmsk_TE_subfamily[,c("subfamily","family","class_update","Total_length")],by=c("subfamily"))
 subfamily_H3K27ac_sample = join(subfamily_H3K27ac_sample_expand,subfamily_H3K27ac_sample,by=c("subfamily","Sample"),type="left")
 colnames(subfamily_H3K27ac_sample)[5] = "Length_ik"
-subfamily_H3K27ac_sample[which(metadata[match(subfamily_H3K27ac_sample$Sample,metadata$Sample),]$chrY == "No"),]$Length_ik = apply(subfamily_H3K27ac_sample[which(metadata[match(subfamily_H3K27ac_sample$Sample,metadata$Sample),]$chrY == "No"),],1,function(x) rmsk_TE_subfamily[match(x[1],rmsk_TE_subfamily$subfamily),]$Total_length_noY)
+subfamily_H3K27ac_sample$Length_ik = ifelse(metadata[match(subfamily_H3K27ac_sample$Sample,metadata$Sample),]$chrY == "Yes",
+                                          subfamily_H3K27ac_sample$Length_ik,
+                                          rmsk_TE_subfamily[match(subfamily_H3K27ac_sample$subfamily,rmsk_TE_subfamily$subfamily),]$Total_length_noY)
 subfamily_H3K27ac_sample[is.na(subfamily_H3K27ac_sample$Length_ijk),]$Length_ijk = 0
 rm(subfamily_H3K27ac_sample_expand)
 
@@ -175,10 +170,7 @@ subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,H3K27ac_stats[,c(1,4)]
 colnames(subfamily_H3K27ac_sample)[7] = "Length_jk"
 
 # Total length of sample
-mnemonics_states_genome = melt(as.matrix(t(read.table("chromHMM/genome/mnemonics_state.txt",sep='\t',header=TRUE,row.names=1)[1,])))[,c(1,3)]
-colnames(mnemonics_states_genome) = c("Sample","Length_k")
-subfamily_H3K27ac_sample = join(subfamily_H3K27ac_sample,mnemonics_states_genome,by=c("Sample"),type="left")
-rm(mnemonics_states_genome)
+subfamily_H3K27ac_sample$Length_k = ifelse(metadata[match(subfamily_H3K27ac_sample$Sample,metadata$Sample),]$chrY == "Yes",GENOME_WIDTH,GENOME_WIDTH_noY)
 
 # LOR enrichment
 subfamily_H3K27ac_sample$Enrichment = log2((subfamily_H3K27ac_sample$Length_ijk/subfamily_H3K27ac_sample$Length_ik)/(subfamily_H3K27ac_sample$Length_jk/subfamily_H3K27ac_sample$Length_k))
@@ -192,8 +184,9 @@ subfamily_H3K27ac_sample = merge(subfamily_H3K27ac_sample,metadata[,c(1,4:9)],by
 # Number of members overlapping H3K27ac peak
 TE_H3K27ac_peaks_members = merge(melt(aggregate(data=TE_H3K27ac_peaks[,c(4,8:105)],.~subfamily,function(x) sum(as.numeric(x))),id.vars=c("subfamily")),melt(aggregate(data=TE_H3K27ac_peaks[,c(4,8:105)],.~subfamily,function(x) sum(as.numeric(x) > 0)),id.vars=c("subfamily")),by=c("subfamily","variable"))
 colnames(TE_H3K27ac_peaks_members)[2:4] = c("Sample","H3K27ac_peaks","Members")
-TE_H3K27ac_peaks_members = merge(TE_H3K27ac_peaks_members,rmsk_TE_subfamily[,c("subfamily","Count")],by="subfamily")
-TE_H3K27ac_peaks_members[which(TE_H3K27ac_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$Count = rmsk_TE_subfamily[match(TE_H3K27ac_peaks_members[which(TE_H3K27ac_peaks_members$Sample %in% c("E116","E117","E123","E124","E126","E127")),]$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY
+TE_H3K27ac_peaks_members$Count = ifelse(metadata[match(TE_H3K27ac_peaks_members$Sample,metadata$Sample),]$chrY == "Yes",
+                                      rmsk_TE_subfamily[match(TE_H3K27ac_peaks_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count,
+                                      rmsk_TE_subfamily[match(TE_H3K27ac_peaks_members$subfamily,rmsk_TE_subfamily$subfamily),]$Count_noY)
 
 # Proportion of members overlapping H3K27ac peak
 TE_H3K27ac_peaks_members$Percent = TE_H3K27ac_peaks_members$Members/TE_H3K27ac_peaks_members$Count
