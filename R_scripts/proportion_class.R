74b91a5dcc15922bdb21123613fd59880c174ec1
Erica Pehrsson <epehrsson@wustl.edu>
2018-03-05 16:52:44 -0600

    Removed IMR90 calculation

--- R_scripts/proportion_class.R
+++ R_scripts/proportion_class.R
@@ -49,10 +49,6 @@ class_CpG_meth_total[,2:5] = class_CpG_meth_total[,2:5]/(rmsk_TE_class[match(cla
 class_CpG_meth_total = melt(class_CpG_meth_total,id.vars="class")
 colnames(class_CpG_meth_total) = c("class","State","Proportion")
 
-# Proportion of class in each state across all samples, no IMR90
-class_CpG_meth_total_noIMR90 = aggregate(data=class_CpG_meth[which(class_CpG_meth$Sample != "E017"),2:6],.~class,sum)
-class_CpG_meth_total_noIMR90[,2:5] = class_CpG_meth_total_noIMR90[,2:5]/(rmsk_TE_class[match(class_CpG_meth_total_noIMR90$class,rmsk_TE_class$class_update),]$CpGs*sample_counts["Include","WGBS"])
-
 # By sample - Add CpGs in class in sample
 class_CpG_meth$CpGs_class = rowSums(class_CpG_meth[,3:6])
 class_CpG_meth = melt(class_CpG_meth,id.vars=c("class","Sample","CpGs_class"))
