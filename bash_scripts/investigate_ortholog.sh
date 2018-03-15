4124cbbf88539c7c2cf3ddd6b12ae55b482cfd8b
Erica Pehrsson <epehrsson@wustl.edu>
2018-03-15 12:19:23 -0500

    Removed write statement (already in Rmd)

--- bash_scripts/investigate_ortholog.sh
+++ bash_scripts/investigate_ortholog.sh
@@ -3,7 +3,6 @@
 
 # hg19 TEs hypomethlated in human/mouse also in promoter state	
 #TE_landscape/Mouse/human_mouse_hypo_1TssA.bed	
-write.table(unique(human_mouse_orthologs_mm10_hypo[which(human_mouse_orthologs_mm10_hypo$Human_state == "1_TssA" & human_mouse_orthologs_mm10_hypo$Mouse_State == "1"),1:7]),row.names=FALSE,col.names=FALSE,quote=FALSE,sep='\t',file="Mouse/human_mouse_hypo_1TssA.bed")
 
 # Overlap with GENCODE promoters	
 #TE_landscape/Mouse/human_mouse_hypo_1TssA_Gencode.txt	
