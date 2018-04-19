ce24e62f9813aee419938a1fcfe89f8d7b67c3ce
Erica Pehrsson <epehrsson@wustl.edu>
2018-04-19 15:55:02 -0500

    RNA-seq with strand-agnostic files; changed normalization

--- R_scripts/compare_marks.R
+++ R_scripts/compare_marks.R
@@ -23,13 +23,3 @@ compare_marks_all$WGBS = factor(compare_marks_all$WGBS,levels=c(meth_states,"Hyp
 compare_marks_all[which(compare_marks_all$WGBS == "Hyper"),]$WGBS = "Hypermethylated"
 compare_marks_all[which(compare_marks_all$WGBS == "Hypo"),]$WGBS = "Hypomethylated"
 compare_marks_all$WGBS = factor(compare_marks_all$WGBS,levels=meth_states)
-
-# Add unique TEs x sample
-test = aggregate(data=compare_marks_all,TE_sample~addNA(WGBS)+addNA(DNase)+addNA(H3K27ac)+addNA(RNA),sum)
-colnames(test)[1:4] = c("WGBS","DNase","H3K27ac","RNA")
-test = merge(compare_marks_unique,test,by=c("WGBS","DNase","H3K27ac","RNA"),all=TRUE)
-colnames(test)[5:6] = c("TE_sample_unique","TE_sample_total")
-test$Normalization = test$TE_sample_unique/test$TE_sample_total
-
-compare_marks_all = merge(compare_marks_all,test[,c(1:4,7)],by=c("WGBS","DNase","H3K27ac","RNA"),all=TRUE)
-compare_marks_all$TE_sample_norm = compare_marks_all$TE_sample/compare_marks_all$Normalization
