b8de8163affb669e904f07f8f4ae2bef0e3a29a6
Erica Pehrsson <epehrsson@wustl.edu>
2018-05-18 14:46:14 -0500

    Updated with new combined matrices and inter state switching metric

--- R_scripts/state_switching.R
+++ R_scripts/state_switching.R
@@ -1,25 +1,15 @@
 # State switching
 # See 7/5/2016, 7/7/2016, 2/6/2017, 2/9/2017
 
+source("R_scripts/potential_class.R")
+
+# chromHMM intra state switching
 # Matrix of states annotated on the same TE in the same sample (intra)
 # Normalized by number of instances (TE x sample) each state is found, alone or with another state
 
-source("R_scripts/potential_class.R")
-
 # Format TEs 
-ss_intra_TE = read.table("chromHMM/state_switching/all_chromHMM_TE_state_intra.txt",sep='\t',header=TRUE,row.names=1)
-ss_intra_TE[1:9,10:15] = t(ss_intra_TE[10:15,1:9])
-ss_intra_TE[lower.tri(ss_intra_TE)] = t(ss_intra_TE)[lower.tri(ss_intra_TE)]
-
-# Format other TEs
-ss_intra_other = read.table("chromHMM/state_switching/all_chromHMM_other_state_intra.txt",sep='\t',header=TRUE,row.names=1)
-ss_intra_other[1:9,10:15] = t(ss_intra_other[10:15,1:9])
-ss_intra_other[lower.tri(ss_intra_other)] = t(ss_intra_other)[lower.tri(ss_intra_other)]
-
-# Combine
-state_switching_intra = ss_intra_TE + ss_intra_other
-rm(ss_intra_TE)
-rm(ss_intra_other)
+state_switching_intra = read.table("chromHMM/state_switching/rmsk_TEother_chromHMM_intra.txt",sep='\t',header=TRUE,row.names=1)
+state_switching_intra[lower.tri(state_switching_intra)] = t(state_switching_intra)[lower.tri(state_switching_intra)]
 
 # For Chi-sq tests
 state_switching_intra_raw = state_switching_intra
@@ -30,33 +20,24 @@ for (i in 1:15){
 }
 
 # chromHMM inter state switching
+# For TEs ever in a state, what proportion of samples do they spend in each state
+
 # Format TEs
-ss_inter_TE = read.table("chromHMM/state_switching/all_chromHMM_TE_state_inter.txt",sep='\t',header=TRUE,row.names=1)
-ss_inter_TE[lower.tri(ss_inter_TE)] = t(ss_inter_TE)[lower.tri(ss_inter_TE)]
-
-# Format other TEs
-ss_inter_other = read.table("chromHMM/state_switching/all_chromHMM_other_state_inter.txt",sep='\t',header=TRUE,row.names=1)
-ss_inter_other[lower.tri(ss_inter_other)] = t(ss_inter_other)[lower.tri(ss_inter_other)]
-
-# Combine
-state_switching_inter = ss_inter_TE + ss_inter_other
-rm(ss_inter_TE)
-rm(ss_inter_other)
+state_switching_inter = read.table("chromHMM/state_switching/rmsk_TEother_chromHMM_inter.txt",sep='\t',header=TRUE,row.names=1)
 
 # For Chi-sq tests
 state_switching_inter_raw = state_switching_inter
 
-# Normalize
+# Normalize row by number of TEs ever in state
 for (i in 1:15){
   state_switching_inter[i,] = state_switching_inter[i,]/apply(chromHMM_TE_state_dist[,2:16],2,function(x) sum(x[2:128]))[i]
 }
 
 # chromHMM, by class
-ss_inter_class = lapply(list.files(path="chromHMM/state_switching/class",full.names=TRUE),function(x) read.table(x,sep='\t',header=TRUE,row.names=1))
+ss_inter_class = lapply(list.files(path="chromHMM/state_switching/class",pattern="rmsk_TEother",full.names=TRUE),function(x) read.table(x,sep='\t',header=TRUE,row.names=1))
 names(ss_inter_class) = c("DNA","LINE","LTR","Other","SINE","SVA")
 
 for (j in 1:6){
- ss_inter_class[[j]][lower.tri(ss_inter_class[[j]])] = t(ss_inter_class[[j]])[lower.tri(ss_inter_class[[j]])]
  for (i in 1:15){
    ss_inter_class[[j]][i,] = ss_inter_class[[j]][i,]/apply(chromHMM_TE_state_class[which(chromHMM_TE_state_class$class_update == names(ss_inter_class)[j]),3:17],2,function(x) sum(x[2:128]))[i]
  }
@@ -78,7 +59,7 @@ ss_inter_meth = ss_inter_meth[c(2,3,1,4),c(2,3,1,4)]
 # For Chi-sq tests
 ss_inter_meth_raw = ss_inter_meth
 
-# Normalize
+# Normalize by number of TEs ever in state
 for (i in 1:4){
   ss_inter_meth[i,] = ss_inter_meth[i,]/apply(TE_meth_average_category[,c(2,4,3,5)],2,function(x) sum(x[2:38]))[i]
 }
