e8cddfe2e254fa031e6dbf9124c6436f90312e33
Erica Pehrsson <epehrsson@wustl.edu>
2018-05-29 14:50:07 -0500

    Added code from Rmd; removed intermediate files; added by-class contribution

--- R_scripts/proportion_class.R
+++ R_scripts/proportion_class.R
@@ -28,8 +28,8 @@ class_chromHMM$Bases_sample = apply(class_chromHMM,1,function(x) mnemonics_state
 class_chromHMM$Enrichment = log2((class_chromHMM$Bases_state_class/class_chromHMM$Bases_class)/(class_chromHMM$Bases_state/class_chromHMM$Bases_sample))
 
 # Proportion of class in each state across all samples
-contribution_class = aggregate(data=class_chromHMM,Bases_state_class~State+class,sum)
-contribution_class$Proportion = apply(contribution_class,1,function(x) as.numeric(x[3])/rmsk_TE_class[match(x[2],rmsk_TE_class$class_update),]$chromHMM_total_width)
+chromHMM_class = aggregate(data=class_chromHMM,Bases_state_class~State+class,sum)
+chromHMM_class$Proportion = apply(chromHMM_class,1,function(x) as.numeric(x[3])/rmsk_TE_class[match(x[2],rmsk_TE_class$class_update),]$chromHMM_total_width)
 
 # WGBS
 # By sample
@@ -119,3 +119,28 @@ TE_H3K27ac_class_total = colSums(dcast(TE_H3K27ac_class[,1:3],Sample~class)[,2:7
 TE_H3K27ac_class_total = melt(as.matrix(TE_H3K27ac_class_total/rmsk_TE_class[match(names(TE_H3K27ac_class_total),rmsk_TE_class$class_update),]$H3K27ac_total_width))
 colnames(TE_H3K27ac_class_total) = c("class","State","Proportion")
 TE_H3K27ac_class_total$State = rep("H3K27ac",6)
+
+
+# Proportion in state by class
+combine_class_proportion = rbind(chromHMM_class[,c(1:2,4)],class_CpG_meth_total,TE_DNase_class_total,TE_H3K27ac_class_total)
+combine_class_proportion$Cohort = factor(c(rep("chromHMM",90),rep("WGBS",24),rep("DNase",6),rep("H3K27ac",6)),levels=c("chromHMM","WGBS","DNase","H3K27ac"))
+rm(list=c("chromHMM_class","class_CpG_meth_total","TE_DNase_class_total","TE_H3K27ac_class_total"))
+
+# By-sample proportion by TE class
+colnames(class_CpG_meth) = gsub("CpGs","Bases",colnames(class_CpG_meth))
+by_sample_class = rbind(class_chromHMM,class_CpG_meth,TE_DNase_class,TE_H3K27ac_class)
+by_sample_class = merge(by_sample_class,metadata[,c("Sample",sample_categories)],by="Sample")
+by_sample_class$Proportion_state = as.numeric(by_sample_class$Proportion_state)
+rm(list=c("class_chromHMM","class_CpG_meth","TE_DNase_class","TE_H3K27ac_class"))
+
+# By-sample contribution (includes duplicate bases)
+contribution_class = ddply(by_sample_class,.(class,State),summarise,Bases_state_class=sum(Bases_state_class))
+## Adding total class width across all samples
+test = rmsk_TE_class[,c("class_update","chromHMM_total_width")]
+test$State = "Total"
+colnames(test)[1:2] = c("class","Bases_state_class")
+contribution_class = rbind(contribution_class,test)
+rm(test)
+## Proportion of TE bases in each class by state (includes duplicate bases)
+contribution_class = ddply(contribution_class,.(State),transform,Bases_state=sum(Bases_state_class))
+contribution_class$Proportion = contribution_class$Bases_state_class/contribution_class$Bases_state
