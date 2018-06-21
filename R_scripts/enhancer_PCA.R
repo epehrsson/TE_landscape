4b5b3653c5bdc74a92cb857717bbff45b0c8744f
Erica Pehrsson <epehrsson@wustl.edu>
2018-06-21 17:07:11 -0500

    Updated to take in long matrix of TE x sample

--- R_scripts/enhancer_PCA.R
+++ R_scripts/enhancer_PCA.R
@@ -2,10 +2,12 @@
 # See 11/3/2016, 11/4/2016, 2/6/2017, 2/9/2017
 
 # Matrix of TEs
-enhancer_matrix = rbind(read.table("chromHMM/enhancer_matrix.txt",sep='\t',header=TRUE),read.table("chromHMM/enhancer_matrix_other.txt",sep='\t',header=TRUE))
-enhancer_matrix = enhancer_matrix[which(apply(enhancer_matrix[,8:134],1,function(x) sum(x > 0)) > 0),]
-enhancer_matrix[which(enhancer_matrix$chromosome == "chrY"),as.vector(metadata[which(metadata$chrY == "No"),]$Sample)] = NA
+enhancer_matrix = read.table("chromHMM/enhancer_matrix.txt",sep='\t')
+colnames(enhancer_matrix) = c(TE_coordinates[c(1:4,6,5,7)],"Sample")
+enhancer_matrix = dcast(enhancer_matrix,chromosome+start+stop+subfamily+family+class+strand~Sample,length)
 rownames(enhancer_matrix) = apply(enhancer_matrix,1,function(x) paste(x[1],x[2],x[3],x[4],x[5],x[6],x[7],sep="_"))
+enhancer_matrix = enhancer_matrix[which(enhancer_matrix$chromosome != "chrY"),8:134]
+enhancer_matrix[enhancer_matrix > 0] = 1
 
 # Compute PCA
-enhancer_matrix_pca = prcomp(t(na.omit(enhancer_matrix[,8:134])),scale=TRUE,center=TRUE)
+enhancer_matrix_pca = prcomp(t(enhancer_matrix),scale=TRUE,center=TRUE)
