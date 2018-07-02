a964b417369e8ecf266c5f48d2e0a22a744c8a97
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-02 12:42:31 -0500

    Updated to match enhancer script, remove chrY

--- R_scripts/PCA.R
+++ R_scripts/PCA.R
@@ -4,18 +4,18 @@
 #load("R_datasets/TE_H3K27ac_peaks.RData")
 
 # Configure matrices
-TE_DNase_binary = TE_DNase_peaks[,1:60]
-TE_DNase_binary[,8:60] = ifelse(TE_DNase_binary[,8:60]>0, 1, 0)
-TE_DNase_binary[which(TE_DNase_binary$chromosome == "chrY"),as.vector(metadata[which(metadata$chrY == "No"),]$Sample)] = NA
-TE_DNase_binary = TE_DNase_binary[which(apply(TE_DNase_binary[,8:60],1,var) != 0),]
+TE_DNase_binary = TE_DNase_peaks[which(TE_DNase_peaks$chromosome != "chrY"),1:60]
 rownames(TE_DNase_binary) = apply(TE_DNase_binary,1,function(x) paste(x[1],x[2],x[3],x[4],x[5],x[6],x[7],sep="_"))
+TE_DNase_binary = TE_DNase_binary[,8:60]
+TE_DNase_binary = ifelse(TE_DNase_binary>0, 1, 0)
+TE_DNase_binary = TE_DNase_binary[which(apply(TE_DNase_binary,1,var) != 0),]
 
-TE_H3K27ac_binary = TE_H3K27ac_peaks[,1:105]
-TE_H3K27ac_binary[,8:105] = ifelse(TE_H3K27ac_binary[,8:105]>0, 1, 0)
-TE_H3K27ac_binary[which(TE_H3K27ac_binary$chromosome == "chrY"),as.vector(metadata[which(metadata$chrY == "No"),]$Sample)] = NA
-TE_H3K27ac_binary = TE_H3K27ac_binary[which(apply(TE_H3K27ac_binary[,8:105],1,var) != 0),]
+TE_H3K27ac_binary = TE_H3K27ac_peaks[which(TE_H3K27ac_peaks$chromosome != "chrY"),1:105]
 rownames(TE_H3K27ac_binary) = apply(TE_H3K27ac_binary,1,function(x) paste(x[1],x[2],x[3],x[4],x[5],x[6],x[7],sep="_"))
+TE_H3K27ac_binary = TE_H3K27ac_binary[,8:105]
+TE_H3K27ac_binary = ifelse(TE_H3K27ac_binary>0, 1, 0)
+TE_H3K27ac_binary = TE_H3K27ac_binary[which(apply(TE_H3K27ac_binary,1,var) != 0),]
 
 # Compute PCA
-DNase_pca = prcomp(t(na.omit(TE_DNase_binary[,8:60])),scale=TRUE,center=TRUE)
-H3K27ac_pca = prcomp(t(na.omit(TE_H3K27ac_binary[,8:105])),scale=TRUE,center=TRUE)
+DNase_pca = prcomp(t(TE_DNase_binary),scale=TRUE,center=TRUE)
+H3K27ac_pca = prcomp(t(TE_H3K27ac_binary),scale=TRUE,center=TRUE)
