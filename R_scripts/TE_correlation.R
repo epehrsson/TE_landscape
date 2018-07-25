c533d598d8aa51babab54579dc9ed214d0cb7260
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-25 13:22:58 -0500

    Switched to proportion of samples

--- R_scripts/TE_correlation.R
+++ R_scripts/TE_correlation.R
@@ -18,24 +18,29 @@ rmsk_TE_measure[is.na(rmsk_TE_measure)] = 0
 # Add number of samples in chromHMM state per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,chromHMM_TE_state[,c(TE_coordinates,chromHMM_states,"States","Max_states_intra")],by=TE_coordinates)
 rmsk_TE_measure = rename(rmsk_TE_measure,c("States"="States.chromHMM"))
+rmsk_TE_measure[,chromHMM_states] = apply(rmsk_TE_measure[,chromHMM_states],2,function(x) as.numeric(ifelse(rmsk_TE_measure$chromosome != "chrY",x/sample_counts["All","chromHMM"],x/sample_counts["chrY","chromHMM"])))
 
 # Add number of samples in methylation state per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,TE_meth_average[,c(TE_coordinates,"CpGs",meth_states,"States")],by=TE_coordinates,all.x=TRUE)
 rmsk_TE_measure[which(is.na(rmsk_TE_measure$CpGs)),]$CpGs = 0
 rmsk_TE_measure$CpGs_per_length = rmsk_TE_measure$CpGs/rmsk_TE_measure$Length
 rmsk_TE_measure = rename(rmsk_TE_measure,c("States"="States.WGBS"))
+rmsk_TE_measure[,meth_states] = apply(rmsk_TE_measure[,meth_states],2,function(x) as.numeric(x/sample_counts["All","WGBS"]))
 
 # Add number of samples overlapping DNase peak per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,TE_DNase_peaks[,c(TE_coordinates,"Samples")],by=TE_coordinates,all.x=TRUE)
 colnames(rmsk_TE_measure)[length(colnames(rmsk_TE_measure))] = "DNase"
 rmsk_TE_measure[which(is.na(rmsk_TE_measure$DNase)),]$DNase = 0
+rmsk_TE_measure$DNase = as.numeric(ifelse(rmsk_TE_measure$chromosome != "chrY",rmsk_TE_measure$DNase/sample_counts["All","DNase"],rmsk_TE_measure$DNase/sample_counts["chrY","DNase"]))
 
 # Add number of samples overlapping H3K27ac peak per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,TE_H3K27ac_peaks[,c(TE_coordinates,"Samples")],by=TE_coordinates,all.x=TRUE)
 colnames(rmsk_TE_measure)[length(colnames(rmsk_TE_measure))] = "H3K27ac"
 rmsk_TE_measure[which(is.na(rmsk_TE_measure$H3K27ac)),]$H3K27ac = 0
+rmsk_TE_measure$H3K27ac = as.numeric(ifelse(rmsk_TE_measure$chromosome != "chrY",rmsk_TE_measure$H3K27ac/sample_counts["All","H3K27ac"],rmsk_TE_measure$H3K27ac/sample_counts["chrY","H3K27ac"]))
 
 # Add number of samples expressed per TE
 rmsk_TE_measure = merge(rmsk_TE_measure,RNA_TE[,c(TE_coordinates,"Expressed_samples","Max_expression")],by=TE_coordinates)
+rmsk_TE_measure$Expressed_samples = as.numeric(ifelse(rmsk_TE_measure$chromosome != "chrY",rmsk_TE_measure$Expressed_samples/sample_counts["All","RNA"],rmsk_TE_measure$Expressed_samples/sample_counts["chrY","RNA"]))
 
 rmsk_TE_measure = rmsk_TE_measure[,c(TE_coordinates,"class_update",measure_metrics,cohorts,states,measure_states_extra)]
