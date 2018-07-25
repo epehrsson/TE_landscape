fbff1ec176d651c4c230cfaf46b5fe6769537993
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-25 15:51:05 -0500

    Added CpGs in each state and average methylation level, RefSeq features with TEs; removed obsolete code

--- bash_scripts/mean_CpG_overlap.sh
+++ bash_scripts/mean_CpG_overlap.sh
@@ -1,19 +1,21 @@
 # Average methylation over merged feature
-# 5/25/2016, 12/14/2016, 6/15/2017, 8/7/2017
+# 5/25/2016, 12/14/2016, 6/15/2017, 8/7/2017, 6/5/2018
 
 # Average methylation level over all CpGs in each sample	 
 #TE_landscape/WGBS/all_CpG_Meth_average_new.txt	
 python ../bin/TE_landscape/average_methylation_overall.py all_CpG_Meth.bed WGBS_samples.txt all_CpG_Meth_distribution_new.txt
-
-#TE_landscape/WGBS/methylation_old/all_CpG_Meth_average.txt		
-python ../bin/TE_landscape/average_methylation_overall.py all_CpG_Meth.bed WGBS_samples.txt
-#TE_landscape/WGBS/methylation_old/error/all_CpG_Meth_average.txt		
 
 # Average methylation level of CpG overlapping TEs	
 #TE_landscape/WGBS/CpG_TE_Meth_average.txt	
 
 # Average methylation level of CpG not overlapping TEs	
 #TE_landscape/WGBS/CpG_noTE_Meth_average.txt	
+
+# Average methylation level of CpGs overlapping merged Refseq features
+awk '{print $0 > $1}' WGBS/feature_CpG_Meth.bed
+for file in refseq_*; do echo $file; python ~/bin/TE_landscape/average_methylation_overall.py $file sample_lists/WGBS_samples.txt $file\_average.txt feature; done 
+python ~/bin/TE_landscape/average_methylation_overall.py intergenic sample_lists/WGBS_samples.txt refseq_intergenic_average.txt feature
+for file in refseq_*_average.txt; do feature=$( basename "$file" _average.txt ); awk -v OFS='\t' -v feature=$feature '{print feature, $0}' $file >> WGBS/feature_CpG_Meth_average.txt; done
 
 # Average methylation level of CpGs overlapping merged Refseq features, no TEs	 
 #TE_landscape/WGBS/Refseq_features/CpG_refseq_[feature]_merge_noTE_Meth_average.txt [6 files]	
--- bash_scripts/state_CpG_overlap.sh
+++ bash_scripts/state_CpG_overlap.sh
@@ -5,26 +5,9 @@
 #TE_landscape/WGBS/all_CpG_Meth_states.txt		 
 awk '{for (i=4;i<=NF;i++){if($i == -1) miss[i]+=1; else if ($i < 0.3) hypo[i]+=1; else if ($i > 0.7) hyper[i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[i]+=1;}}; END{for (i in hyper) print i" "hypo[i]" "inter[i]" "hyper[i]" "miss[i];}' all_CpG_Meth.bed > all_CpG_Meth_states.txt
 
-#Proportion of all CpGs in each methylation state
-awk '{for (i=4;i<=NF;i++){if($i == -1) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/all_CpG_Meth.bed
-awk '{for (i=4;i<=NF;i++){if(($i != -1) && ($i < 0.3)) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/all_CpG_Meth.bed
-awk '{for (i=4;i<=NF;i++){if($i > 0.7) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/a   ll_CpG_Meth.bed
-awk '{for (i=4;i<=NF;i++){if(($i <= 0.7) && ($i >= 0.3)) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}   ' ~/TE_landscape/all_CpG_Meth.bed
-
 # Number of CpGs in each state, TE CpGs, each sample
 #TE_landscape/WGBS/CpG_TE_Meth_states.txt		 
 awk '{for (i=4;i<=NF;i++){if($i == -1) miss[i]+=1; else if ($i < 0.3) hypo[i]+=1; else if ($i > 0.7) hyper[i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[i]+=1;}}; END{for (i in hyper) print i" "hypo[i]" "inter[i]" "hyper[i]" "miss[i];}' CpG_TE_Meth.bed > CpG_TE_Meth_states.txt
-
-# Proportion of TE CpGs in each methylation state
-awk '{for (i=4;i<=NF;i++){if($i == -1) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/CpG_TE_Meth.bed
-awk '{for (i=4;i<=NF;i++){if(($i != -1) && ($i < 0.3)) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/CpG_TE_Meth.bed
-awk '{for (i=4;i<=NF;i++){if($i > 0.7) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/CpG_TE_Meth.bed
-awk '{for (i=4;i<=NF;i++){if(($i <= 0.7) && ($i >= 0.3)) sum[i]+=1;}}; END{for (i in sum) print "for column "i" is " sum[i];}' ~/TE_landscape/CpG_TE_Meth.bed
-
-# Number of hypomethylated CpGs per class per sample
-#TE_landscape/WGBS/class/TE_class_hypo.txt
-while read line; do awk -v OFS='\t' -v class=$line '{if($5 == class) print $0}' TE_CpG_Meth_new.bed | cut -f8- - | sort | uniq | awk -v OFS='\t' -v class=$line '{for (i=4;i<=NF;i++){if(($i != -1) && ($i < 0.3)) sum[i]+=1;}}; END{for (i in sum) print i, sum[i], class;}' -; done < TEother_class.txt >>TE_class_hypo.txt
-awk -v OFS='\t' '{if(($5 == "LINE?") || ($5 == "SINE?") || ($5 == "DNA?") || ($5 == "LTR?") || ($5 == "Unknown?") || ($5 == "Unknown")) print $0}' TE_CpG_Meth_new.bed | cut -f8- - | sort | uniq | awk -v OFS='\t' '{for (i=4;i<=NF;i++){if(($i != -1) && ($i < 0.3)) sum[i]+=1;}}; END{for (i in sum) print i, sum[i], "Unconfident";}' - >> TE_class_hypo.txt
 
 # Number of CpGs in each state, TE CpGs, by sample x class
 #TE_landscape/WGBS/class_CpG_Meth_states.txt		 
@@ -32,14 +15,13 @@ while read line; do awk -v OFS='\t' -v class=$line '{if($5 == class) print $0}' 
 awk -v OFS='\t' '{if(($5 == "LINE?") || ($5 == "SINE?") || ($5 == "DNA?") || ($5 == "LTR?") || ($5 == "Unknown?") || ($5 == "Unknown")) print $0}' TE_CpG_Meth_new.bed | cut -f8- - | sort | uniq | awk -v OFS='\t' '{for (i=4;i<=NF;i++){if($i == -1) miss[i]+=1; else if ($i < 0.3) hypo[i]+=1; else if ($i > 0.7) hyper[i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[i]+=1;}}; END{for (i in hyper) print i, hypo[i], inter[i], hyper[i], miss[i], "Unconfident";}' - >> TE_class_hypo.txt (class_CpG_Meth_states.txt)
 awk -v OFS='\t' '{if(($5 == "LINE?") || ($5 == "SINE?") || ($5 == "DNA?") || ($5 == "LTR?") || ($5 == "Unknown?") || ($5 == "Unknown") || ($5 == "RC")) print $0}' TE_CpG_Meth_new.bed | cut -f8- - | sort | uniq | awk -v OFS='\t' '{for (i=4;i<=NF;i++){if($i == -1) miss[i]+=1; else if ($i < 0.3) hypo[i]+=1; else if ($i > 0.7) hyper[i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[i]+=1;}}; END{for (i=4;i<=NF;i++) {print i, hypo[i], inter[i], hyper[i], miss[i], "Unconfident_RC";}}' - >> class_CpG_Meth_states.txt
 
-# Number of hypomethylated CpGs per subfamily per sample
-#TE_landscape/WGBS/subfamily/TE_subfamily_hypo.txt		 
-while read line; do awk -v OFS='\t' -v subfam=$line '{if($4 == subfam) print $0}' TE_CpG_Meth_new.bed | cut -f8- - | sort | uniq | awk -v OFS='\t' -v subfam=$line '{for (i=4;i<=NF;i++){if(($i != -1) && ($i < 0.3)) sum[i]+=1;}}; END{for (i in sum) print i, sum[i], subfam;}' -; done < TE_subfamilies.txt >> TE_subfamily_hypo.txt
-
 # Number of CpGs in each state, TE CpGs, by sample x subfamily
 #TE_landscape/WGBS/subfamily_CpG_Meth_states.txt	
 while read line; do awk -v OFS='\t' -v subfam=$line '{if($4 == subfam) print $0}' TE_CpG_Meth_new.bed | cut -f8- - | sort | uniq | awk -v OFS='\t' -v subfam=$line '{for (i=4;i<=40;i++){if($i == -1) miss[i]+=1; else if ($i < 0.3) hypo[i]+=1; else if ($i > 0.7) hyper[i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[i]+=1;}}; END{for (i=4;i<=40;i++){print i, hypo[i], inter[i], hyper[i], miss[i], subfam;}}' -; done < ../features/TEs/subfamily/subfamilies.txt >> subfamily_CpG_Meth_states.txt
 
 # Number of CpGs in each state, CpGs overlapping Refseq features, each sample
+awk -v OFS='\t' '{for (i=5;i<=NF;i++){if($i == -1) miss[$1, i]+=1; else if ($i < 0.3) hypo[$1, i]+=1; else if ($i > 0.7) hyper[$1, i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[$1, i]+=1}}END{for(i in hypo){split(i,sep,SUBSEP); print "Hypomethylated", sep[1], sep[2], hypo[i]}; for(i in hyper){split(i,sep,SUBSEP); print "Hypermethylated", sep[1], sep[2], hyper[i]}; for(i in inter){split(i,sep,SUBSEP); print "Intermediate", sep[1], sep[2], inter[i]}; for(i in miss){split(i,sep,SUBSEP); print "Missing", sep[1], sep[2], miss[i]}}' WGBS/feature_CpG_Meth.bed > WGBS/feature_CpG_Meth_states.txt
+
+# Number of CpGs in each state, CpGs overlapping Refseq features, no TEs, each sample
 #TE_landscape/WGBS/Refseq_features/CpG_feature_Meth_states.txt
  for file in CpG_refseq_*_noTE_Meth.bed; do awk -v OFS='\t' -v feature=$(basename "$file" .bed) '{for (i=4;i<=NF;i++){if($i == -1) miss[i]+=1; else if ($i < 0.3) hypo[i]+=1; else if ($i > 0.7) hyper[i]+=1; else if (($i <= 0.7) && ($i >= 0.3)) inter[i]+=1;}}; END{for (i=4;i<=NF;i++){print i, hypo[i], inter[i], hyper[i], miss[i], feature;}}' $file >>  CpG_feature_Meth_states.txt; done
