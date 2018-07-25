a4ad998a49caec7aaf8bfc779abb8ca213ae57ad
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-25 16:46:31 -0500

    ChromHMM block length by sample; added number of blocks per sample

--- bash_scripts/peak_widths.sh
+++ bash_scripts/peak_widths.sh
@@ -1,8 +1,12 @@
 # Peak/state widths
 # 4/27/2016, 7/31/2017, 8/3/2017
 
+# ChromHMM, block size by state and sample
 #TE_landscape/chromHMM/chromHMM_blocks.txt
-for file in chromHMM_bedfiles/*.bed; do awk '{print ($3-$2), $4}' $file >> chromHMM_blocks.txt ; done
+while read line; do awk -v OFS='\t' -v sample=$line '{print $3-$2, $4, sample}' raw_data/chromHMM/$line\_15_coreMarks_mnemonics.bed >> chromHMM/chromHMM_blocks.txt; done < sample_lists/mnemonics.txt
+
+## Number of blocks per sample
+while read line; do awk -v sample=$line -v OFS='\t' '{state[$4]+=1}END{for(i in state){print sample, i, state[i]}}' raw_data/chromHMM/$line\_15_coreMarks_mnemonics.bed >> chromHMM/chromHMM_peaks.txt; done < sample_lists/mnemonics.txt
 
 #TE_landscape/DNase/peak_widths.txt
 while read line; do awk -v OFS='\t' -v sample=$line '{print $3-$2, sample}' ../raw_data/DNase/DNase_narrow_peaks/$line\-DNase.macs2.narrowPeak >> peak_widths.txt; done < ../sample_lists/DNase_samples.txt
