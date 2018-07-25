e78c9952aee8c81f4b9ea0b5a1211079a1a7fd3a
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-25 13:18:06 -0500

    Switched to sample proportion, added CGate subfamilies to rmsk_TE_subfamily, and added subfamily correlation code to an R script; removed SD and switched to median for all subfamily metrics; added proportion of each subfamily on each chromosome

--- R_scripts/TE_correlation_subfamily.R
+++ R_scripts/TE_correlation_subfamily.R
@@ -6,17 +6,15 @@
 
 # Adding number of samples enriched in each state
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily,
-                                  dcast(subfamily_state_sample_counts,class_update+family+subfamily~State,value.var=c("V1"))[,c("subfamily",chromHMM_states,meth_states,"DNase","H3K27ac")],
+                                  dcast(subfamily_state_sample_counts,subfamily~State,
+                                        value.var=c("Sample.Proportion"))[,c("subfamily",chromHMM_states,meth_states,"DNase","H3K27ac")],
                                   by="subfamily",all.x=TRUE)
 
 # Adding number of samples >1% in each state
-test = dcast(subfamily_state_sample_counts_pc,class_update+family+subfamily~State,value.var=c("V1"))[,c("subfamily",chromHMM_states,meth_states,"DNase","H3K27ac")]
+test = dcast(subfamily_state_sample_counts_pc,subfamily~State,
+             value.var=c("Sample.Proportion"))[,c("subfamily",chromHMM_states,meth_states,"DNase","H3K27ac")]
 colnames(test)[2:22] = lapply(colnames(test)[2:22],function(x) paste(x,"PC",sep="_"))
 rmsk_TE_subfamily_measure = merge(rmsk_TE_subfamily_measure,test,by="subfamily",all.x=TRUE)
 rm(test)
-
-# Adding C-GATE
-rmsk_TE_subfamily_measure$CGate = rep("no",968)
-rmsk_TE_subfamily_measure[which(rmsk_TE_subfamily_measure$subfamily %in% cgate_subfams),]$CGate = "yes"
 
 contrasts(rmsk_TE_subfamily_measure$class_update) <- contr.sum
--- R_scripts/TE_subfamily_stats.R
+++ R_scripts/TE_subfamily_stats.R
@@ -8,16 +8,19 @@
 # Number of instances, median/sd of length, mean/sd mappability and age by subfamily
 rmsk_TE_subfamily = ddply(rmsk_TE,~subfamily+family+class_update,summarize,
                           Count = length(Length),
-                          Median_length = median(Length), 
-                          SD_length = sd(Length), 
-                          Mappability = mean(mappability), 
-                          Mappability_SD = sd(mappability), 
-                          Age = mean(JC_distance), 
-                          Age_SD = sd(JC_distance))
-rmsk_TE_subfamily = merge(rmsk_TE_subfamily,ddply(rmsk_TE[which(rmsk_TE$chromosome != "chrY"),],~subfamily+family+class_update,summarize,Count_noY = length(Length)),by=c("subfamily","family","class_update"))
+                          Length = median(Length), 
+                          Mappability = median(mappability), 
+                          Age = median(JC_distance))
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,
+                          ddply(rmsk_TE[which(rmsk_TE$chromosome != "chrY"),],~subfamily+family+class_update,
+                                summarize,Count_noY = length(Length)),by=c("subfamily","family","class_update"))
 
-# Proportion of each subfamily in each feature
-rmsk_TE_subfamily = merge(rmsk_TE_subfamily,ddply(rmsk_TE,~subfamily,function(y) apply(y[,13:33],2,function(x) length(na.omit(x))/length(x))),by="subfamily")
+# Total length of subfamily
+subfamily_length = cbind(read.table("features/TEs/subfamily/subfamily_lengths.txt",sep='\t'),
+                         read.table("features/TEs/subfamily/subfamily_lengths_noY.txt",sep='\t'))[,c(1:2,4)]
+colnames(subfamily_length) = c("subfamily","Total_length","Total_length_noY")
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,subfamily_length,by="subfamily")
+rm(subfamily_length)
 
 # Number of CpGs per subfamily
 TE_subfamily_CpG_count = read.table("WGBS/subfamily/TE_CpG_subfamily.txt",sep='\t')
@@ -27,6 +30,9 @@ rmsk_TE_subfamily[which(is.na(rmsk_TE_subfamily$CpGs)),]$CpGs = 0
 rmsk_TE_subfamily$CpGs = rmsk_TE_subfamily$CpGs/2
 rm(TE_subfamily_CpG_count)
 
+rmsk_TE_subfamily$CpGs_per_TE = rmsk_TE_subfamily$CpGs/rmsk_TE_subfamily$Count
+rmsk_TE_subfamily$CpGs_per_kbp = rmsk_TE_subfamily$CpGs/(rmsk_TE_subfamily$Total_length/1000)
+
 # Number of TEs with CpGs
 TE_CpG_count = read.table("WGBS/TE_CpG_count.txt",sep='\t')
 colnames(TE_CpG_count) = c(TE_coordinates[c(1:4,6,5,7)],"CpGs")
@@ -35,11 +41,18 @@ colnames(subfamily_wCpG_count)[2] = "Count_CpGs"
 rmsk_TE_subfamily = merge(rmsk_TE_subfamily,subfamily_wCpG_count,by="subfamily",all.x=TRUE)
 rm(list=c("TE_CpG_count","subfamily_wCpG_count"))
 
-# Total length of subfamily
-subfamily_length = cbind(read.table("features/TEs/subfamily/subfamily_lengths.txt",sep='\t'),read.table("features/TEs/subfamily/subfamily_lengths_noY.txt",sep='\t'))[,c(1:2,4)]
-colnames(subfamily_length) = c("subfamily","Total_length","Total_length_noY")
-rmsk_TE_subfamily = merge(rmsk_TE_subfamily,subfamily_length,by="subfamily")
-rm(subfamily_length)
+# Proportion of each subfamily in each feature
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,ddply(rmsk_TE,~subfamily,function(y) 
+  apply(y[,cohorts],2,function(x) length(na.omit(x))/length(x))),by="subfamily")
 
-rmsk_TE_subfamily$CpGs_per_TE = rmsk_TE_subfamily$CpGs/rmsk_TE_subfamily$Count
-rmsk_TE_subfamily$CpGs_per_kbp = rmsk_TE_subfamily$CpGs/(rmsk_TE_subfamily$Total_length/1000)
+# Proportion of each subfamily on each chromosome
+rmsk_TE_chromosome = ddply(rmsk_TE,.(subfamily,chromosome),summarise,Chr=length(chromosome))
+rmsk_TE_chromosome = ddply(rmsk_TE_chromosome,.(subfamily),transform,Chr_percent=Chr/sum(Chr))
+rmsk_TE_chromosome = dcast(rmsk_TE_chromosome,subfamily~chromosome,value.var="Chr_percent")
+rmsk_TE_chromosome[is.na(rmsk_TE_chromosome)] = 0
+rmsk_TE_subfamily = merge(rmsk_TE_subfamily,rmsk_TE_chromosome,by="subfamily")
+rm(rmsk_TE_chromosome)
+
+# Adding C-GATE
+rmsk_TE_subfamily$CGate = rep("no",968)
+rmsk_TE_subfamily[which(rmsk_TE_subfamily$subfamily %in% cgate_subfams),]$CGate = "yes"
