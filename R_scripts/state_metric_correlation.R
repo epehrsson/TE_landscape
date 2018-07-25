c30ebd02394d7f2ad88136b7075a6ad69e814387
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-25 13:24:11 -0500

    Reverted to remove error

--- R_scripts/state_metric_correlation.R
+++ R_scripts/state_metric_correlation.R
@@ -21,8 +21,9 @@ correlate_TE_state$class_update = factor(correlate_TE_state$class_update,levels=
 correlate_TE_feature = rbind(correlate_spearman(rmsk_TE_measure,"Length",measure_metrics[2:5]),
                              correlate_spearman(rmsk_TE_measure,"mappability",measure_metrics[3:5]), 
                              correlate_spearman(rmsk_TE_measure,"JC_distance",measure_metrics[4:5]),
-                             correlate_spearman(rmsk_TE_measure,"CpGs",measure_metrics[5]))
-correlate_TE_feature$Metric = factor(c(rep("Length",28),rep("mappability",21),rep("JC_distance",14),rep("CpGs",14)),levels=c("Length","mappability","JC_distance","CpGs","CpGs_per_length"))
+                             correlate_spearman(rmsk_TE_measure,"CpGs",measure_metrics[4:5]))
+correlate_TE_feature$Metric = factor(c(rep("Length",28),rep("mappability",21),rep("JC_distance",14),rep("CpGs",14)),
+                                     levels=c("Length","mappability","JC_distance","CpGs","CpGs_per_length"))
 correlate_TE_feature$p.value = as.numeric(as.character(correlate_TE_feature$p.value))
 correlate_TE_feature$estimate.rho = as.numeric(as.character(correlate_TE_feature$estimate.rho))
 correlate_TE_feature$class_update = factor(correlate_TE_feature$class_update,levels=c("DNA","LINE","LTR","SINE","SVA","Other","All"))
