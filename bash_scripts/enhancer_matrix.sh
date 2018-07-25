e333137bf01675ce29a8cd6195e14fa3c993bb64
Erica Pehrsson <epehrsson@wustl.edu>
2018-07-25 14:01:24 -0500

    Replaced with TEs in 6_EnhG/7_Enh state per sample (from histone profile). Removed state and bp information (converted to binary matrix in R)

--- bash_scripts/enhancer_matrix.sh
+++ bash_scripts/enhancer_matrix.sh
@@ -1,8 +1,6 @@
 # Binary matrix of TEs x sample indicating if TE is in genic or regular enhancer state
 # 11/3/2016, 2/2/2017
+# Updated 6/21/18
 
-#TE_landscape/chromHMM/enhancer_matrix.txt	
-python ../bin/TE_landscape/create_state_matrix.py all_chromHMM_TE_sorted.txt rmsk_TE.txt enhancer_states.txt mnemonics.txt enhancer_matrix.txt			
-
-#TE_landscape/chromHMM/enhancer_matrix_other.txt	
-python ../bin/TE_landscape/create_state_matrix.py all_chromHMM_other_sorted.txt rmsk_other.txt enhancer_states.txt mnemonics.txt enhancer_matrix_other.txt
+# From files with TEs in the 6_EnhG/7_Enh state per sample (combine marks)
+cat chromHMM/enhancer_matrix/* | cut -f1-8 - > chromHMM/enhancer_matrix.txt
