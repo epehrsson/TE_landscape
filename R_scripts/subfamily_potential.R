c61cf9cb1addd3b875414bf6fd96627a1ea7c771
Erica Pehrsson <epehrsson@wustl.edu>
2018-09-12 14:55:37 -0500

    Normalized subfamily potential by number of samples per metric

--- R_scripts/subfamily_potential.R
+++ R_scripts/subfamily_potential.R
@@ -2,6 +2,11 @@
 
 subfamily_state_potential = ddply(subfamily_state_sample_combined,.(subfamily,State),summarise,Samples=sum(Length_ijk > 0))
 subfamily_state_potential$State = factor(subfamily_state_potential$State,levels=states[1:21])
+subfamily_state_potential$Sample.Proportion = ifelse(subfamily_state_potential$State %in% chromHMM_states,subfamily_state_potential$Samples/sample_counts["All","chromHMM"],
+       ifelse(subfamily_state_potential$State %in% meth_states,subfamily_state_potential$Samples/sample_counts["All","WGBS"],
+              ifelse(subfamily_state_potential$State == "DNase",subfamily_state_potential$Samples/sample_counts["All","DNase"],
+                     ifelse(subfamily_state_potential$State == "H3K27ac",subfamily_state_potential$Samples/sample_counts["All","H3K27ac"],"NA"))))
+subfamily_state_potential$Sample.Proportion = as.numeric(subfamily_state_potential$Sample.Proportion)
 
 subfam_states = merge(ddply(subfamily_state_potential,.(subfamily),summarise,States=length(unique(State[which(Samples > 0)]))),
                       ddply(ddply(subfamily_state_sample_combined,.(subfamily,Sample),summarise,Intra=length(unique(State[which(Length_ijk > 0)]))),
