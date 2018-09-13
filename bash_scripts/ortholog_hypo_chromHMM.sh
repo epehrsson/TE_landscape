2d120a4b1d404464420a9cfc86fd432624dbd478
Erica Pehrsson <epehrsson@wustl.edu>
2018-09-13 15:29:25 -0500

    Updated to pull out chromHMM state for all orthologous human TEs, relevant samples

--- bash_scripts/ortholog_hypo_chromHMM.sh
+++ bash_scripts/ortholog_hypo_chromHMM.sh
@@ -1,18 +1,10 @@
-# Human-mouse hypomethylated orthologs, chromHMM
+# Human-mouse orthologs, chromHMM
 # 5/29/2017, 5/30/2017, 7/26/2017, 7/31/2017, 10/3/2017, 10/10/2017
 
-# hg19 TEs hypomethylated in human and mouse	
-#Mouse/hg19_ortholog_hypo.txt	
-
-# mm10 TEs hypomethylated in human and mouse
-#Mouse/mm10_ortholog_hypo.txt
-
 # hg19 chromHMM
-## Pull out human chromHMM state for hypomethylated hg19 TEs
-awk -v OFS='\t' '{if($11 == "Hypo") print $0}' compare_marks/TE_combine_marks.txt > Mouse/TE_combine_marks_meth.txt
+## chromHMM state for all orthologous hg19 TEs
+## Confirmed that it only pulled out TEs with matching subfamily and that the number of unique TEs is 269096 for all
+cut -f5-11 Mouse/liftover/hg19_mm10_TE_intersect_same.bed | uniq | sort -k1,1 -k2,2n > Mouse/hg19_orthologs.txt
+for file in E066 E071 E081 E083 E084 E086 E088 E092 E094 E096 E105 E106; do tail -n +2 /scratch/ecp/pandas/$file | sort -k1,1 -k2,2n - | bedtools intersect -sorted -wo -a Mouse/hg19_orthologs.txt -b - -f 1 -r > Mouse/hg19_ortholog_chromHMM_$file\.txt; done
+for file in Mouse/chromHMM/hg19_ortholog_chromHMM_*.txt; do cut -f8-18 $file >> Mouse/chromHMM/hg19_chromHMM_TE.txt; done
 
-## Human chromHMM state for hg19 TEs hypomethylated in mouse/human
-python ~/bin/TE_landscape/get_TE_sample.py Mouse/TE_combine_marks_meth.txt Mouse/hg19_ortholog_hypo.txt Mouse/hg19_ortholog_hypo_chromHMM.txt 8
-
-# mm10 chromHMM
-## Mouse chromHMM state for mm10 TEs hypomethylated in human/mouse
