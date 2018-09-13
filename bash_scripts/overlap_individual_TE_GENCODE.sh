0fd2536dd228386aaf2bc12a5236b94bdadf1fa8
Erica Pehrsson <epehrsson@wustl.edu>
2018-09-13 15:22:42 -0500

    Removed obsolete subfamily candidate scripts

--- bash_scripts/overlap_individual_TE_GENCODE.sh
+++
@@ -1,9 +0,0 @@
-# GENCODE overlap
-
-state=$1
-
-# Overlap of TEs in state when enriched with GENCODE promoters (2000bp upstream, 500bp downstream TSS)
-for file in enrichment/*_$state\_enriched.bed; do sort -k1,1V -k2,2 $file | bedtools intersect -wo -a - -b ~/genic_features/Gencode/Gencodev19_promoter.txt >> enrichment/Gencodev19_promoter_$state\_enriched.txt; done
-
-# Overlap of TEs in state when enriched with GENCODE genes (2000 bp upstream, 500bp downstream gene)
-for file in enrichment/*_$state\_enriched.bed; do sort -k1,1V -k2,2 $file | bedtools intersect -wo -a - -b ~/genic_features/Gencode/Gencode_v19_genes_margin.txt >> enrichment/Gencodev19_genes_$state\_enriched.txt; done
