3453e406da9b47c3f317d360367fe5378aba1f74
Erica Pehrsson <epehrsson@wustl.edu>
2018-09-21 09:06:21 -0500

    Updated HOMER scripts

--- bash_scripts/run_homer_examples.sh
+++
@@ -1,5 +0,0 @@
- findMotifsGenome.pl enrichment	LTR12C_1_TssA_enrich0_BLOOD.bed hg19 enrichment	HOMER_LTR12C_1_TssA_enrich0_BLOOD -size given -nomotif -bg enrichment	LTR12C_no1_TssA.bed
- findMotifsGenome.pl enrichment	LTR22A_7_Enh_enrich0_Digestive.bed hg19 enrichment	HOMER_LTR22A_7_Enh_enrich0_Digestive -size given -nomotif -bg enrichment	LTR22A_no7_Enh.bed
- findMotifsGenome.pl enrichment	MER130_7_Enh_enrich0_Brain.bed hg19 enrichment	HOMER_MER130_7_Enh_enrich0_Brain -size given -nomotif -bg enrichment	MER130_no7_Enh.bed
- findMotifsGenome.pl enrichment	MER53_1_TssA_enrich0_Blood.bed hg19 enrichment	HOMER_MER53_1_TssA_enrich0_Blood -size given -nomotif -bg enrichment	MER53_no1_TssA.bed
- findMotifsGenome.pl enrichment	LFSINE_Vert_7_Enh_enrich0_Brain.bed hg19 enrichment	HOMER_LFSINE_Vert_7_Enh_enrich0_Brain -size given -nomotif -bg enrichment	LFSINE_Vert_no7_Enh.bed
