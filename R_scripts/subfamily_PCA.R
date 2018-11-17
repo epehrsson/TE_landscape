a251773f6c57cf50f1572598d92e09c8d4aded3b
Erica Pehrsson <epehrsson@wustl.edu>
2018-11-17 17:10:27 -0600

    Replaced tSNE with PCA (by subfamily and sample)

--- R_scripts/subfamily_PCA.R
+++ R_scripts/subfamily_PCA.R
@@ -1,97 +1,100 @@
-# Perform PCA/tSNE at the subfamily level
-
-set.seed(42)
+# Perform PCA at the subfamily level
 
 # chromHMM 3D clustering - by sample
-sample_chromHMM_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% chromHMM_states),c("subfamily","State","Sample","Enrichment")],
+sample_chromHMM_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% chromHMM_states & 
+                                                                   rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count > THRESHOLD_IK_MEMBER),
+                                                           c("subfamily","State","Sample","Enrichment")],
                                       subfamily+State~Sample,value.var = "Enrichment")
 rownames(sample_chromHMM_3D) = paste(sample_chromHMM_3D$subfamily,sample_chromHMM_3D$State,sep="_")
 sample_chromHMM_3D = sample_chromHMM_3D[,3:129]
 sample_chromHMM_3D = sample_chromHMM_3D[which(apply(sample_chromHMM_3D,1,var) != 0),]
 
 # chromHMM 3D clustering - by subfamily
-subfamily_chromHMM_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% chromHMM_states),c("subfamily","State","Sample","Enrichment")],
+subfamily_chromHMM_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% chromHMM_states & 
+                                                                      rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count > THRESHOLD_IK_MEMBER),
+                                                              c("subfamily","State","Sample","Enrichment")],
                            Sample+State~subfamily,value.var = "Enrichment")
 rownames(subfamily_chromHMM_3D) = paste(subfamily_chromHMM_3D$Sample,subfamily_chromHMM_3D$State,sep="_")
-subfamily_chromHMM_3D = subfamily_chromHMM_3D[,3:970]
+subfamily_chromHMM_3D = subfamily_chromHMM_3D[,3:939]
 subfamily_chromHMM_3D = subfamily_chromHMM_3D[which(apply(subfamily_chromHMM_3D,1,var) != 0),]
 
-## 7_Enh 3D clustering
-subfamily_7_Enh_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "7_Enh"),c("subfamily","Sample","Enrichment")],
-                                   subfamily~Sample,value.var = "Enrichment")
-rownames(subfamily_7_Enh_enrichment) = subfamily_7_Enh_enrichment$subfamily
-subfamily_7_Enh_enrichment = subfamily_7_Enh_enrichment[,2:128]
-subfamily_7_Enh_enrichment = subfamily_7_Enh_enrichment[which(apply(subfamily_7_Enh_enrichment,1,var) != 0),]
-
 # WGBS 3D clustering - by sample
-sample_WGBS_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% meth_states),c("subfamily","State","Sample","Enrichment")],
+sample_WGBS_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% meth_states & 
+                                                               rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count_CpGs > THRESHOLD_IK_MEMBER),
+                                                       c("subfamily","State","Sample","Enrichment")],
                            subfamily+State~Sample,value.var = "Enrichment")
 rownames(sample_WGBS_3D) = paste(sample_WGBS_3D$subfamily,sample_WGBS_3D$State,sep="_")
 sample_WGBS_3D = sample_WGBS_3D[,3:39]
 sample_WGBS_3D = sample_WGBS_3D[which(apply(sample_WGBS_3D,1,var) != 0),]
 
 # WGBS 3D clustering - by subfamily
-subfamily_WGBS_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% meth_states),c("subfamily","State","Sample","Enrichment")],
+subfamily_WGBS_3D = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State %in% meth_states & 
+                                                                  rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count_CpGs > THRESHOLD_IK_MEMBER),
+                                                          c("subfamily","State","Sample","Enrichment")],
                               Sample+State~subfamily,value.var = "Enrichment")
 rownames(subfamily_WGBS_3D) = paste(subfamily_WGBS_3D$Sample,subfamily_WGBS_3D$State,sep="_")
-subfamily_WGBS_3D = subfamily_WGBS_3D[,3:967]
+subfamily_WGBS_3D = subfamily_WGBS_3D[,3:916]
 subfamily_WGBS_3D = subfamily_WGBS_3D[which(apply(subfamily_WGBS_3D,1,var) != 0),]
 
-## DNase
-subfamily_DNase_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "DNase"),c("subfamily","Sample","Enrichment")],
-                                   subfamily~Sample,value.var = "Enrichment")
-rownames(subfamily_DNase_enrichment) = subfamily_DNase_enrichment$subfamily
-subfamily_DNase_enrichment = subfamily_DNase_enrichment[,2:54]
+## DNase - by sample
+sample_DNase_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "DNase" & 
+                                                                        rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count > THRESHOLD_IK_MEMBER),
+                                                                c("subfamily","Sample","Enrichment")],
+                                subfamily~Sample,value.var = "Enrichment")
+rownames(sample_DNase_enrichment) = sample_DNase_enrichment$subfamily
+sample_DNase_enrichment = sample_DNase_enrichment[,2:54]
+sample_DNase_enrichment = sample_DNase_enrichment[which(apply(sample_DNase_enrichment,1,var) != 0),]
+
+## DNase - by subfamily
+subfamily_DNase_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "DNase" & 
+                                                                           rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count > THRESHOLD_IK_MEMBER),
+                                                                   c("subfamily","Sample","Enrichment")],
+                                   Sample~subfamily,value.var = "Enrichment")
+rownames(subfamily_DNase_enrichment) = subfamily_DNase_enrichment$Sample
+subfamily_DNase_enrichment = subfamily_DNase_enrichment[,2:938]
 subfamily_DNase_enrichment = subfamily_DNase_enrichment[which(apply(subfamily_DNase_enrichment,1,var) != 0),]
 
-## H3K27ac
-subfamily_H3K27ac_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "H3K27ac"),c("subfamily","Sample","Enrichment")],
-                                     subfamily~Sample,value.var = "Enrichment")
-rownames(subfamily_H3K27ac_enrichment) = subfamily_H3K27ac_enrichment$subfamily
-subfamily_H3K27ac_enrichment = subfamily_H3K27ac_enrichment[,2:99]
+## H3K27ac - by sample
+sample_H3K27ac_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "H3K27ac" & 
+                                                                          rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count > THRESHOLD_IK_MEMBER),
+                                                                  c("subfamily","Sample","Enrichment")],
+                                subfamily~Sample,value.var = "Enrichment")
+rownames(sample_H3K27ac_enrichment) = sample_H3K27ac_enrichment$subfamily
+sample_H3K27ac_enrichment = sample_H3K27ac_enrichment[,2:99]
+sample_H3K27ac_enrichment = sample_H3K27ac_enrichment[which(apply(sample_H3K27ac_enrichment,1,var) != 0),]
+
+## H3K27ac - by subfamily
+subfamily_H3K27ac_enrichment = dcast(subfamily_state_sample_combined[which(subfamily_state_sample_combined$State == "H3K27ac" & 
+                                                                             rmsk_TE_subfamily[match(subfamily_state_sample_combined$subfamily,rmsk_TE_subfamily$subfamily),]$Count > THRESHOLD_IK_MEMBER),
+                                                                     c("subfamily","Sample","Enrichment")],
+                                     Sample~subfamily,value.var = "Enrichment")
+rownames(subfamily_H3K27ac_enrichment) = subfamily_H3K27ac_enrichment$Sample
+subfamily_H3K27ac_enrichment = subfamily_H3K27ac_enrichment[,2:938]
 subfamily_H3K27ac_enrichment = subfamily_H3K27ac_enrichment[which(apply(subfamily_H3K27ac_enrichment,1,var) != 0),]
 
 # PCA
-sample_chromHMM_pca = prcomp(t(sample_chromHMM_3D),scale=TRUE,center=TRUE)
-sample_WGBS_pca = prcomp(t(sample_WGBS_3D),scale=TRUE,center=TRUE)
-subfamily_DNase_pca = prcomp(t(subfamily_DNase_enrichment),scale=TRUE,center=TRUE)
-subfamily_H3K27ac_pca = prcomp(t(subfamily_H3K27ac_enrichment),scale=TRUE,center=TRUE)
-
-sample_7_Enh_pca = prcomp(t(subfamily_7_Enh_enrichment),scale=TRUE,center=TRUE)
-
-# tSNE
 ## Samples
-## chromHMM
-sample_chromHMM_tsne = run_tsne(t(sample_chromHMM_3D),perplex = 10)
-sample_chromHMM_tsne = merge(sample_chromHMM_tsne,metadata[,c("Sample",sample_categories)],by.x="object",by.y="Sample",all.x=TRUE)
+sample_chromHMM_pca = prcomp(t(sample_chromHMM_3D),scale=TRUE,center=TRUE)
+sample_chromHMM_pca = format_pca(sample_chromHMM_pca,metadata,"Sample")
 
-## WGBS
-sample_WGBS_tsne = run_tsne(t(sample_WGBS_3D),perplex = 10)
-sample_WGBS_tsne = merge(sample_WGBS_tsne,metadata[,c("Sample",sample_categories)],by.x="object",by.y="Sample",all.x=TRUE)
+sample_WGBS_pca = prcomp(t(sample_WGBS_3D),scale=TRUE,center=TRUE)
+sample_WGBS_pca = format_pca(sample_WGBS_pca,metadata,"Sample")
 
-## DNase
-sample_DNase_tsne = run_tsne(t(subfamily_DNase_enrichment),perplex = 10)
-sample_DNase_tsne = merge(sample_DNase_tsne,metadata[,c("Sample",sample_categories)],by.x="object",by.y="Sample",all.x=TRUE)
-sample_DNase_tsne = merge(sample_DNase_tsne,DNase_stats[,c("Sample","Peaks")],by.x="object",by.y="Sample",all.x=TRUE)
+sample_DNase_pca = prcomp(t(sample_DNase_enrichment),scale=TRUE,center=TRUE)
+sample_DNase_pca = format_pca(sample_DNase_pca,metadata,"Sample")
 
-## H3K27ac
-sample_H3K27ac_tsne = run_tsne(t(subfamily_H3K27ac_enrichment),perplex = 10)
-sample_H3K27ac_tsne = merge(sample_H3K27ac_tsne,metadata[,c("Sample",sample_categories)],by.x="object",by.y="Sample",all.x=TRUE)
-sample_H3K27ac_tsne = merge(sample_H3K27ac_tsne,H3K27ac_stats[,c("Sample","Peaks")],by.x="object",by.y="Sample",all.x=TRUE)
+sample_H3K27ac_pca = prcomp(t(sample_H3K27ac_enrichment),scale=TRUE,center=TRUE)
+sample_H3K27ac_pca = format_pca(sample_H3K27ac_pca,metadata,"Sample")
 
-## Subfamilies
-## chromHMM
-subfamily_chromHMM_tsne = run_tsne(t(subfamily_chromHMM_3D),perplex = 30, the = 0.5)
-subfamily_chromHMM_tsne = merge(subfamily_chromHMM_tsne,rmsk_TE_subfamily[,c("subfamily","family","class_update")],by.x="object",by.y="subfamily",all.x=TRUE)
+## Subfamily
+subfamily_chromHMM_pca = prcomp(t(subfamily_chromHMM_3D),scale=TRUE,center=TRUE)
+subfamily_chromHMM_pca = format_pca(subfamily_chromHMM_pca,rmsk_TE_subfamily,"subfamily")
 
-## WGBS
-subfamily_WGBS_tsne = run_tsne(t(subfamily_WGBS_3D),perplex = 30, the = 0.5)
-subfamily_WGBS_tsne = merge(subfamily_WGBS_tsne,rmsk_TE_subfamily[,c("subfamily","family","class_update")],by.x="object",by.y="subfamily",all.x=TRUE)
+subfamily_WGBS_pca = prcomp(t(subfamily_WGBS_3D),scale=TRUE,center=TRUE)
+subfamily_WGBS_pca = format_pca(subfamily_WGBS_pca,rmsk_TE_subfamily,"subfamily")
 
-## DNase
-subfamily_DNase_tsne = run_tsne(subfamily_DNase_enrichment,perplex = 30, the = 0.5)
-subfamily_DNase_tsne = merge(subfamily_DNase_tsne,rmsk_TE_subfamily[,c("subfamily","family","class_update")],by.x="object",by.y="subfamily",all.x=TRUE)
+subfamily_DNase_pca = prcomp(t(subfamily_DNase_enrichment),scale=TRUE,center=TRUE)
+subfamily_DNase_pca = format_pca(subfamily_DNase_pca,rmsk_TE_subfamily,"subfamily")
 
-## H3K27ac
-subfamily_H3K27ac_tsne = run_tsne(subfamily_H3K27ac_enrichment,perplex = 30, the = 0.5)
-subfamily_H3K27ac_tsne = merge(subfamily_H3K27ac_tsne,rmsk_TE_subfamily[,c("subfamily","family","class_update")],by.x="object",by.y="subfamily",all.x=TRUE)
+subfamily_H3K27ac_pca = prcomp(t(subfamily_H3K27ac_enrichment),scale=TRUE,center=TRUE)
+subfamily_H3K27ac_pca = format_pca(subfamily_H3K27ac_pca,rmsk_TE_subfamily,"subfamily")
