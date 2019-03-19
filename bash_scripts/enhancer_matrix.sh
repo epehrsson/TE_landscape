03556b070e8e7ea18b6bd5aec608810344c24053
Erica Pehrsson <epehrsson@wustl.edu>
2019-03-19 13:13:03 -0500

    Removed loose bash scripts (code now included in Rmd)

--- bash_scripts/enhancer_matrix.sh
+++
@@ -1,6 +0,0 @@
-# Binary matrix of TEs x sample indicating if TE is in genic or regular enhancer state
-# 11/3/2016, 2/2/2017
-# Updated 6/21/18
-
-# From files with TEs in the 6_EnhG/7_Enh state per sample (combine marks)
-cat chromHMM/enhancer_matrix/* | cut -f1-8 - > chromHMM/enhancer_matrix.txt
--- bash_scripts/investigate_always.sh
+++
@@ -1,8 +0,0 @@
-# TEs always in a state
-
-# Find the nearest gene
-sort -k1,1 -k2,2n always_TEs.bed > always_TEs_sorted.bed
-sort -k1,1 -k2,2n ~/genic_features/RefSeq/refseq_genes.txt | bedtools closest -a always_TEs_sorted.bed -b - -D b -t all > always_TEs_genes.txt
-
-# Find the distance to the nearest gene
-awk -v FS='\t' '{if(($8 == "DNase") && ($21 > 5000)) print $0}' always_TEs_genes.txt | wc -l
