18f7a5523d6585fb02cc510a0f9ab5155dde2eb1
Erica Pehrsson <epehrsson@wustl.edu>
2019-03-19 15:07:25 -0500

    Added raw mouse wget back into bash script

---
+++ bash_scripts/get_mouse_chromHMM.sh
@@ -0,0 +1,12 @@
+wget https://www.encodeproject.org/files/ENCFF580WIS/@@download/ENCFF580WIS.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF520TYE/@@download/ENCFF520TYE.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF360BWA/@@download/ENCFF360BWA.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF850SZS/@@download/ENCFF850SZS.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF027XQM/@@download/ENCFF027XQM.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF750AZO/@@download/ENCFF750AZO.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF410WGK/@@download/ENCFF410WGK.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF445BPN/@@download/ENCFF445BPN.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF882VQM/@@download/ENCFF882VQM.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF580YIU/@@download/ENCFF580YIU.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF786MCR/@@download/ENCFF786MCR.bed.gz .
+wget https://www.encodeproject.org/files/ENCFF491PKK/@@download/ENCFF491PKK.bed.gz .
