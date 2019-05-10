95407ed588708423bf0cae7212f77bba73bc8402
Erica Pehrsson <epehrsson@wustl.edu>
2019-05-10 11:05:36 -0500

    Removed addition of total samples in state, H3K27ac, samples in group by state; added 200bp windows in active reg states

--- R_scripts/replicate_matrices.R
+++ R_scripts/replicate_matrices.R
@@ -1,9 +1,9 @@
 # Load TE x sample x state
-# Includes all TEs in the state in each sample, plus total samples in state per TE
 print("Load state matrices")
 
 ## chromHMM
-chromHMM_active_matrix = lapply(states[c(1:3,6:7)],function(x) load_state(x))
+chromHMM_active_matrix = lapply(states[c(1:3,6:7)],function(x) read.table(paste("chromHMM/chromHMM_",x,".txt",sep=""),sep='\t',
+                                                                          col.names=c(TE_coordinates[c(1:4,6,5,7)],"Sample","Overlap","State","Category"))[,c(1:8)])
 names(chromHMM_active_matrix) = states[c(1:3,6:7)]
 chromHMM_active_matrix = ldply(chromHMM_active_matrix)
 colnames(chromHMM_active_matrix)[1] = "State"
@@ -15,11 +15,6 @@ colnames(H3K27ac_pairs)[8:10] = c("Total","Sample","Peaks")
 H3K27ac_pairs = H3K27ac_pairs[which(H3K27ac_pairs$Peaks > 0),c(TE_coordinates,"Total","Sample")]
 H3K27ac_pairs$State = rep("H3K27ac",dim(H3K27ac_pairs)[1])
 
-chromHMM_active_matrix = rbind(chromHMM_active_matrix,H3K27ac_pairs)
-
-# Generate matrices of TE x samples in Group by state
-print("Add metadata")
-chromHMM_active_group = merge(chromHMM_active_matrix,metadata[,c("Sample","Group")],by="Sample")
-
-print("Count samples in group")
-chromHMM_active_group = ddply(chromHMM_active_group,.(State,chromosome,start,stop,subfamily,family,class,strand,Group),summarise,Group.Count=length(Group))
+# Load whole genome
+windows_active = read.table("chromHMM/genome/windows/windows_active_reg.bed",sep='\t')
+colnames(windows_active) = c("chr","start","stop","State","Sample")
