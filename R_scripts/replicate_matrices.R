5da4ac35ac01624fd502fc01481321482a37a091
Erica Pehrsson <epehrsson@wustl.edu>
2019-08-01 13:03:41 -0500

    Removed H3K27ac matrix step

--- R_scripts/replicate_matrices.R
+++ R_scripts/replicate_matrices.R
@@ -8,13 +8,6 @@ names(chromHMM_active_matrix) = states[c(1:3,6:7)]
 chromHMM_active_matrix = ldply(chromHMM_active_matrix)
 colnames(chromHMM_active_matrix)[1] = "State"
 
-## H3K27ac
-H3K27ac_pairs = melt(TE_H3K27ac_peaks[,c(TE_coordinates,as.vector(metadata[which(!is.na(metadata$H3K27ac)),]$Sample),"Samples")],
-                     id.vars=c(TE_coordinates,"Samples"))
-colnames(H3K27ac_pairs)[8:10] = c("Total","Sample","Peaks")
-H3K27ac_pairs = H3K27ac_pairs[which(H3K27ac_pairs$Peaks > 0),c(TE_coordinates,"Total","Sample")]
-H3K27ac_pairs$State = rep("H3K27ac",dim(H3K27ac_pairs)[1])
-
 # Load whole genome
 windows_active = read.table("chromHMM/genome/windows/windows_active_reg.bed",sep='\t')
 colnames(windows_active) = c("chr","start","stop","State","Sample")
